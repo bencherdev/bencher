55ff694ca47a8f9ae7dbeec4d5f8ae9f4401a0bd
epompeii <epompeii@protonmail.com>
2020-07-26 19:39:16 -0400

    Stub out Terms and Conditions page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -18,6 +18,7 @@ const IndexPage = () => (
     <Link to="/client/">Client Side</Link> <br />
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="/terms/">Terms of Use</Link>
   </Layout>
 )
 
---
+++ client/src/pages/terms.tsx
@@ -0,0 +1,14 @@
+// If you don't want to use TypeScript you can delete this file!
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TermsPage = () => (
+  <Layout>
+    <SEO title="TableFlow Terms of Use" />
+    <h1>TableFlow Terms of Use</h1>
+  </Layout>
+)
+
+export default TermsPage
