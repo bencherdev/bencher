4554f777d96eedc196e8c517708f084aaaee8d3d
epompeii <epompeii@protonmail.com>
2020-07-29 17:42:49 -0400

    Create conditionl arrow start and end logic

--- client/src/components/studio/modeler/arrow.tsx
+++ client/src/components/studio/modeler/arrow.tsx
@@ -1,17 +1,76 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 const Arrow = (props: { source: any; destination: any }) => {
-  let start = props?.source?.position?.x
-  if (props?.source?.type === "new") {
-    console.log(props.source?.dimensions?.radius)
-    start += props.source?.dimensions?.radius / 2
+  const [path, setPath] = useState({
+    source: {},
+    destination: {},
+  })
+  const [points, setPoints] = useState("")
+
+  function handlePath() {
+    setPath({ source: props.source, destination: props.destination })
+    setPoints(positions())
   }
-  start = start.toString()
-  let end = props?.destination?.position?.x?.toString()
-  let y = props?.source?.position?.y?.toString()
-  console.log(start)
-  console.log(end)
-  return <line x1={start} y1={y} x2={end} y2={y} stroke="black" />
+
+  function positions() {
+    let start
+    switch (props?.source?.type) {
+      case "new":
+      case "subflow":
+        start = {
+          x: props?.source?.position?.x + props?.source?.dimensions?.radius,
+          y: 0,
+        }
+        break
+      case "parent":
+      case "table":
+      case "decision":
+      case "function":
+      default:
+        start = {
+          x: props?.source?.position?.x + props?.source?.dimensions?.width,
+          y: 0,
+        }
+    }
+
+    let end
+    switch (props?.destination?.type) {
+      case "subflow":
+      case "return":
+        end = {
+          x:
+            props?.destination?.position?.x -
+            props?.destination?.dimensions?.radius,
+          y: 0,
+        }
+        break
+      case "table":
+      case "decision":
+      case "function":
+      default:
+        end = { x: props?.destination?.position?.x, y: 0 }
+    }
+
+    let y = props?.source?.position?.y
+    start.y = y
+    end.y = y
+    return positionString(start) + " " + positionString(end)
+  }
+
+  function positionString(position: { x: number; y: number }) {
+    return position?.x + "," + position?.y
+  }
+
+  useEffect(() => {
+    if (
+      props?.source !== path.source ||
+      props?.destination !== path.destination
+    ) {
+      handlePath()
+    }
+  }, [])
+
+  return <polyline points={points} stroke="black" />
 }
 
 export default Arrow
