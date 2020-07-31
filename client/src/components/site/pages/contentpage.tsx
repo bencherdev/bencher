e4ae3b319ebd2120d837e52074d6416c556763b8
epompeii <epompeii@protonmail.com>
2020-07-31 18:43:04 -0400

    Add ContentPage

---
+++ client/src/components/site/pages/contentpage.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import PropTypes from "prop-types"
+import {
+  Section,
+  Container,
+  Columns,
+  Content,
+  Heading,
+} from "react-bulma-components"
+
+const ContentPage = ({ children }: any) => {
+  return (
+    <Section>
+      <Container>
+        <Columns>
+          <Columns.Column>
+            <Content>{children}</Content>
+          </Columns.Column>
+        </Columns>
+      </Container>
+    </Section>
+  )
+}
+
+ContentPage.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default ContentPage
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,12 +5,15 @@ import "react-bulma-components/dist/react-bulma-components.min.css"
 import "../styles/_variables.sass"
 
 import SitePage from "../components/site/pages/sitepage"
+import ContentPage from "../components/site/pages/contentpage"
 
 const RootIndex = () => (
   <SitePage link={Link}>
-    <h1>TableFlow</h1>
-    <Link to="/studio/">Studio</Link> <br />
-    <Link to="/404">404</Link> <br />
+    <ContentPage>
+      <h1>TableFlow</h1>
+      <Link to="/studio/">Studio</Link> <br />
+      <Link to="/404">404</Link> <br />
+    </ContentPage>
   </SitePage>
 )
 
