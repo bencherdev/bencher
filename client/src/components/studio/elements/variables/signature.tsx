9b145860541d283fd0b57177af59b86e7c96dccc
epompeii <epompeii@protonmail.com>
2020-08-18 19:19:15 -0400

    Temporary breaking move for git tracking

---
+++ client/src/components/studio/elements/variables/signature.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { Table } from "react-bulma-components"
+
+const Signature = (props: {
+  id: string
+  value: any
+  disabled: boolean
+  handleElement: Function
+}) => {
+  return (
+    <div className="table-container">
+      <Table bordered={true} striped={false}>
+        <thead>
+          <tr>
+            <th
+              style={{
+                colSpan: 1,
+                textAlign: "center",
+              }}
+            >
+              Function Signature
+            </th>
+          </tr>
+          <tr>
+            <th>Some Input</th>
+            <th>Some Output</th>
+          </tr>
+        </thead>
+        <tbody>
+          <td>Table Template</td>
+          <td>Table Template</td>
+        </tbody>
+      </Table>
+    </div>
+  )
+}
+
+export default Signature
