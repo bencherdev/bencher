1bad3c786f5dd35e206899ab5ebb3edb57a73cc9
epompeii <epompeii@protonmail.com>
2021-04-20 21:44:03 -0400

    Make home dynamic

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,4 +1,49 @@
+// (Lines like the one below ignore selected Clippy rules
+//  - it's useful when you want to check your code with `cargo make verify`
+// but some rules are too "annoying" or are not applicable for your case.)
+#![allow(clippy::wildcard_imports)]
+
 use seed::{prelude::*, *};
+
+use crate::studio::table::Table;
+
+// ------ ------
+//     Init
+// ------ ------
+
+// `init` describes what should happen when your app started.
+pub fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
+    Model::default()
+}
+
+// ------ ------
+//     Model
+// ------ ------
+
+// `Model` describes our app state.
+pub type Model = i32;
+
+// ------ ------
+//    Update
+// ------ ------
+
+// (Remove the line below once any of your `Msg` variants doesn't implement `Copy`.)
+#[derive(Copy, Clone)]
+// `Msg` describes the different events you can modify state with.
+pub enum Msg {
+    Increment,
+}
+
+// `update` describes how to handle each `Msg`.
+pub fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::Increment => *model += 1,
+    }
+}
+
+// ------ ------
+//     View
+// ------ ------
 
 pub fn view<Ms>() -> Node<Ms> {
     div![
