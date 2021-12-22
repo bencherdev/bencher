8677dc3725046a61962844fe3a38c7e3c4910700
Everett Pompeii <epompeii@protonmail.com>
2021-12-22 09:48:45 -0500

    rm tmpdir

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,7 +1,19 @@
 use sycamore::prelude::*;
 
 fn main() {
-    sycamore::render(|| view! {
-        p { "Hello, World!" }
+    sycamore::render(|| {
+        let count = Signal::new(vec![1, 2]);
+
+        view! {
+            ul {
+                Keyed(KeyedProps {
+                    iterable: count.handle(),
+                    template: |x| view! {
+                        li { (x) }
+                    },
+                    key: |x| *x,
+                })
+            }
+        }
     });
 }
