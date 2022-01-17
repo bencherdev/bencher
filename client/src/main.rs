68c40f91de5cb81143a722efa9f98f834ea814f7
Everett Pompeii <epompeii@protonmail.com>
2022-01-17 05:16:01 -0500

    hooks

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,44 +1,25 @@
-use yew::prelude::*;
+use yew::{function_component, html, use_state, Callback};
 
-enum Msg {
-    AddOne,
-}
+#[function_component(UseState)]
+fn state() -> Html {
+    let counter = use_state(|| 0);
 
-struct Model {
-    value: i64,
-}
+    let onclick = {
+        let counter = counter.clone();
+        Callback::from(move |_| counter.set(*counter + 1))
+    };
 
-impl Component for Model {
-    type Message = Msg;
-    type Properties = ();
-
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self { value: 0 }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
-    }
-
-    fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
-        html! {
-            <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
-            </div>
-        }
+    html! {
+        <div>
+            <button {onclick}>{ "Increment value" }</button>
+            <p>
+                <b>{ "Current value: " }</b>
+                { *counter }
+            </p>
+        </div>
     }
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    yew::start_app::<UseState>();
 }
