f27692f52f3e9ccc6d115fe48865b16ba67314e8
Everett Pompeii <epompeii@protonmail.com>
2022-01-17 16:49:19 -0500

    institution cards

--- client/src/institutions.rs
+++ client/src/institutions.rs
@@ -3,6 +3,7 @@ use yew::{function_component, html, use_state, Callback, Html, Properties};
 
 use rollback::account::{Account, AccountKind, Accounts};
 use rollback::institution::{Institution, Institutions};
+use rollback::total::Total;
 
 #[derive(Properties, PartialEq)]
 pub struct InstitutionsListProps {
@@ -20,14 +21,33 @@ pub fn institutions_list(props: &InstitutionsListProps) -> Html {
 
     html! {
         <div>
-        <div id="institutions">
-            {
-                props.institutions.iter().map(|(institution, _accounts)| {
-                    html!{<div key={ institution.name() }>{ institution.name() }</div>}
-                }).collect::<Html>()
-            }
-        </div>
-        <button class="button" onclick={onclick} >{ "+Add" }</button>
+            <div id="institutions">
+                {
+                    props.institutions.iter().map(|(institution, accounts)| {
+                        html!{
+                            <div>
+                            <InstitutionCard
+                                key={institution.name()}
+                                institution={institution.clone()}
+                                accounts={accounts.clone()}
+                            />
+                            <br/>
+                            </div>
+                        }
+                    }).collect::<Html>()
+                }
+            </div>
+            <div class="card">
+                <div class="card-content">
+                    <div class="content">
+                        <button class="button is-fullwidth" onclick={onclick}>{ "+Add" }</button>
+                    </div>
+
+                    <p class="card-header-icon">
+                        { *add_institution }
+                    </p>
+                </div>
+            </div>
         </div>
     }
 }
@@ -35,16 +55,27 @@ pub fn institutions_list(props: &InstitutionsListProps) -> Html {
 #[derive(Properties, PartialEq)]
 pub struct InstitutionCardProps {
     pub institution: Institution,
+    pub accounts: Accounts,
 }
 
 #[function_component(InstitutionCard)]
 pub fn institution_card(props: &InstitutionCardProps) -> Html {
     html! {
         <div class="card">
-            <div class="card-content">
-                <div class="content">
-                    <button class="button">{ "+Add" }</button>
-                </div>
+            <div class="card-header">
+                <p class="card-header-title">
+                    { props.institution.to_string() }
+                </p>
+
+                <p class="card-header-icon">
+                    { props.accounts.total() }
+                </p>
+
+                <button class="card-header-icon" aria-label="See Accounts">
+                    <span class="icon">
+                        <i class="fas fa-angle-down" aria-hidden="true"></i>
+                    </span>
+                </button>
             </div>
         </div>
     }
