ae23b2d4ce5980d084be4b1844f860bc9c876982
Everett Pompeii <epompeii@protonmail.com>
2022-04-14 05:44:41 -0400

    test

--- cli/src/adapter/rust.rs
+++ cli/src/adapter/rust.rs
@@ -27,3 +27,15 @@ fn parse_stdout(input: &str) -> IResult<&str, Report> {
         |_| todo!(),
     )(input)
 }
+
+#[cfg(test)]
+mod test {
+    use super::parse_stdout;
+
+    #[test]
+    fn test_adapter_rust() {
+        let input = "\nrunning 2 tests\ntest tests::ignored ... ignored\ntest tests::benchmark ... bench:       3,161 ns/iter (+/- 975)\n\ntest result: ok. 0 passed; 0 failed; 1 ignored; 1 measured; 0 filtered out; finished in 0.11s\n\n";
+        let adapted = parse_stdout(input).unwrap();
+        println!("{:?}", adapted);
+    }
+}
