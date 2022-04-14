e6a90e6c901e0c9a103f6e44a05691753917532c
Everett Pompeii <epompeii@protonmail.com>
2022-04-14 17:59:20 -0400

    abc

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -6,6 +6,7 @@ mod adapter;
 mod args;
 mod command;
 mod error;
+mod tests;
 
 use crate::command::Command;
 use crate::error::CliError;
@@ -18,27 +19,4 @@ fn main() -> Result<(), CliError> {
     println!("{report:?}");
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use test::{black_box, Bencher};
-
-    #[test]
-    fn ignored() {
-        assert!(true);
-    }
-
-    #[bench]
-    fn benchmark(b: &mut Bencher) {
-        let x: f64 = 211.0 * 11.0;
-        let y: f64 = 301.0 * 103.0;
-
-        b.iter(|| {
-            // Inner closure, the actual test
-            for _ in 1..10000 {
-                black_box(x.powf(y).powf(x));
-            }
-        });
-    }
 }
