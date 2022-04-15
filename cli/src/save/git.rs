91a2647a5b4c74f955498cb401ed4ca6dfc862ea
Everett Pompeii <epompeii@protonmail.com>
2022-04-15 18:11:13 -0400

    commit

--- cli/src/save/git.rs
+++ cli/src/save/git.rs
@@ -1,8 +1,8 @@
 use std::fs;
 use std::path::Path;
 
-use git2::Repository;
-use git2::Signature;
+use git2::{Commit, ObjectType};
+use git2::{Oid, Repository, Signature};
 use tempfile::tempdir;
 
 use crate::adapter::Report;
@@ -49,20 +49,41 @@ impl Git {
         let path = into.join(&bencher_file);
         fs::write(path, &report)?;
 
-        let mut index = repo.index()?;
-        index.add_path(bencher_file)?;
-        index.write()?;
+        // let mut index = repo.index()?;
+        // index.add_path(bencher_file)?;
+        // index.write()?;
+        let oid = Self::add(&repo, &bencher_file)?;
 
-        let signature = self.signature(&repo)?;
-
-        let message = if let Some(message) = &self.message {
-            message
-        } else {
-            BENCHER_MESSAGE
-        };
-        // repo.commit_create_buffer(&signature, &signature, message)?;
+        let commit = self.commit(&repo, oid)?;
 
         Ok(())
+    }
+
+    fn add(repo: &Repository, path: &Path) -> Result<Oid, git2::Error> {
+        let mut index = repo.index()?;
+        index.add_path(path)?;
+        index.write_tree()
+    }
+
+    // https://zsiciarz.github.io/24daysofrust/book/vol2/day16.html
+    fn commit(&self, repo: &Repository, oid: Oid) -> Result<Oid, git2::Error> {
+        let signature = self.signature(repo)?;
+        let message = self.message.as_deref().unwrap_or(BENCHER_MESSAGE);
+        let tree = repo.find_tree(oid)?;
+        let parent_commit = Self::last_commit(repo);
+        let parents = if let Ok(parent) = &parent_commit {
+            vec![parent]
+        } else {
+            Vec::new()
+        };
+        repo.commit(
+            Some("HEAD"), //  point HEAD to our new commit
+            &signature,   // author
+            &signature,   // committer
+            message,      // commit message
+            &tree,        // tree
+            &parents,     // parents
+        )
     }
 
     fn signature(&self, repo: &Repository) -> Result<Signature, git2::Error> {
@@ -74,5 +95,11 @@ impl Git {
             }
         }
         repo.signature()
+    }
+
+    fn last_commit(repo: &Repository) -> Result<Commit, git2::Error> {
+        let obj = repo.head()?.resolve()?.peel(ObjectType::Commit)?;
+        obj.into_commit()
+            .map_err(|_| git2::Error::from_str("Couldn't find last commit"))
     }
 }
