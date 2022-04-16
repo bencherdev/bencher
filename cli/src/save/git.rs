6b679fc4a3e1bed667d9dc36c543a90246fb78e3
Everett Pompeii <epompeii@protonmail.com>
2022-04-16 07:49:09 -0400

    method/assoc func

--- cli/src/save/git.rs
+++ cli/src/save/git.rs
@@ -64,7 +64,7 @@ impl Git {
     fn clone(&self, into: &Path) -> Result<Repository, git2::Error> {
         // Prepare fetch options.
         let mut fo = git2::FetchOptions::new();
-        fo.remote_callbacks(callbacks(self.key.as_deref(), false));
+        fo.remote_callbacks(self.callbacks(false));
 
         // Prepare builder.
         let mut builder = git2::build::RepoBuilder::new();
@@ -85,7 +85,7 @@ impl Git {
         let signature = self.signature(repo)?;
         let message = self.message.as_deref().unwrap_or(BENCHER_MESSAGE);
         let tree = repo.find_tree(oid)?;
-        let parent_commit = Self::last_commit(repo);
+        let parent_commit = last_commit(repo);
         let parents = if let Ok(parent) = &parent_commit {
             vec![parent]
         } else {
@@ -112,55 +112,51 @@ impl Git {
         repo.signature()
     }
 
-    fn last_commit(repo: &Repository) -> Result<Commit, git2::Error> {
-        let obj = repo.head()?.resolve()?.peel(ObjectType::Commit)?;
-        obj.into_commit()
-            .map_err(|_| git2::Error::from_str("Couldn't find last commit"))
-    }
-
     fn push(&self, repo: &Repository) -> Result<(), git2::Error> {
         // Connect remote.
         let mut remote = repo.find_remote("origin")?;
-        remote.connect_auth(
-            Direction::Push,
-            Some(callbacks(self.key.as_deref(), false)),
-            None,
-        )?;
+        remote.connect_auth(Direction::Push, Some(self.callbacks(false)), None)?;
 
         // Prepare push options.
         let mut po = git2::PushOptions::new();
 
         // Prepare callbacks.
-        po.remote_callbacks(callbacks(self.key.as_deref(), true));
+        po.remote_callbacks(self.callbacks(true));
 
         let url: [&str; 1] = ["refs/heads/master:refs/heads/master"];
 
         // Push remote.
         remote.push(&url, Some(&mut po))
     }
+
+    fn callbacks(&self, update_reference: bool) -> RemoteCallbacks<'_> {
+        let mut callbacks = if let Some(key) = self.key.as_deref() {
+            let mut callbacks = RemoteCallbacks::new();
+            callbacks.credentials(|_url, username_from_url, _allowed_types| {
+                Cred::ssh_key(username_from_url.unwrap(), None, Path::new(key), None)
+            });
+            callbacks
+        } else {
+            RemoteCallbacks::new()
+        };
+        if update_reference {
+            callbacks.push_update_reference(move |name, status| {
+                if let Some(status) = status {
+                    Err(git2::Error::from_str(&format!(
+                        "Update reference failed: {status}"
+                    )))
+                } else {
+                    println!("Push commit for refspec {name} succeeded");
+                    Ok(())
+                }
+            });
+        }
+        callbacks
+    }
 }
 
-fn callbacks(key: Option<&str>, update_reference: bool) -> RemoteCallbacks<'_> {
-    let mut callbacks = if let Some(key) = key {
-        let mut callbacks = RemoteCallbacks::new();
-        callbacks.credentials(|_url, username_from_url, _allowed_types| {
-            Cred::ssh_key(username_from_url.unwrap(), None, Path::new(key), None)
-        });
-        callbacks
-    } else {
-        RemoteCallbacks::new()
-    };
-    if update_reference {
-        callbacks.push_update_reference(move |name, status| {
-            if let Some(status) = status {
-                Err(git2::Error::from_str(&format!(
-                    "Update reference failed: {status}"
-                )))
-            } else {
-                println!("Push commit for refspec {name} succeeded");
-                Ok(())
-            }
-        });
-    }
-    callbacks
+fn last_commit(repo: &Repository) -> Result<Commit, git2::Error> {
+    let obj = repo.head()?.resolve()?.peel(ObjectType::Commit)?;
+    obj.into_commit()
+        .map_err(|_| git2::Error::from_str("Couldn't find last commit"))
 }
