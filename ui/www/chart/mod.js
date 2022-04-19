35b82d60191918da89d87812cdd33525ec4a2daa
Everett Pompeii <epompeii@protonmail.com>
2022-04-18 20:30:06 -0400

    chart mod

---
+++ ui/www/chart/mod.js
@@ -0,0 +1,32 @@
+import { Reports } from "ui";
+import { GroupedBarChart } from "./grouped_bar_chart";
+import * as d3 from "d3";
+const queryString = require('query-string');
+
+export function chart() {
+  var inventory = [];
+  var data = [];
+
+  const reports_arg = queryString.parse(location.search).reports;
+  if (reports_arg) {
+    const reports = Reports.from_str(reports_arg);
+    const inv_data = reports.latency();
+    inventory = JSON.parse(inv_data.inventory());
+    data = JSON.parse(inv_data.data());
+  }
+
+  const d3_chart = document.getElementById("d3-chart");
+  const chart = GroupedBarChart(data, {
+    x: d => d.date_time,
+    y: d => d.duration,
+    z: d => d.name,
+    xLabel: "Date and Time",
+    yLabel: "Time (ns)",
+    zDomain: inventory,
+    colors: d3.schemeTableau10,
+    width: 640,
+    height: 500
+  })
+  d3.select(d3_chart).node().appendChild(chart);
+}
+
--- ui/www/index.js
+++ ui/www/index.js
@@ -1,29 +1,3 @@
-import { Reports } from "ui";
-import { GroupedBarChart } from "./bar_chart";
-import * as d3 from "d3";
-const queryString = require('query-string');
+import { chart } from "./chart/mod.js";
 
-var inventory = [];
-var data = [];
-
-const reports_arg = queryString.parse(location.search).reports;
-if (reports_arg) {
-  const reports = Reports.from_str(reports_arg);
-  const inv_data = reports.latency();
-  inventory = JSON.parse(inv_data.inventory());
-  data = JSON.parse(inv_data.data());
-}
-
-const d3_chart = document.getElementById("d3-chart");
-const chart = GroupedBarChart(data, {
-  x: d => d.date_time,
-  y: d => d.duration,
-  z: d => d.name,
-  xLabel: "Date and Time",
-  yLabel: "Time (ns)",
-  zDomain: inventory,
-  colors: d3.schemeTableau10,
-  width: 640,
-  height: 500
-})
-d3.select(d3_chart).node().appendChild(chart);
+chart();
