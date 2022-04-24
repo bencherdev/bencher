df1af0e1b2aba3db71577c4684a7c1e7d3b43d35
Everett Pompeii <epompeii@protonmail.com>
2022-04-23 21:44:00 -0400

    panel_block

--- ui/www/panel/mod.js
+++ ui/www/panel/mod.js
@@ -1,22 +1,23 @@
 export function panel(inventory) {
+  console.log(inventory);
   const bencher_panel = document.getElementById("bencher-panel");
   bencher_panel.innerHTML = `
   <nav class="panel">
     <p class="panel-heading">
         Bencher
     </p>
-    <label class="panel-block is-active">
-        <input type="checkbox">
-        i am active
-    </label>
-    <label class="panel-block">
-        <input type="checkbox">
-        i am not active
-    </label>
-    <label class="panel-block">
-        <input type="checkbox">
-        i am not active
-    </label>
+    ${(function panel_block() {
+      var inventory_blocks = "";
+      for (let i = 0; i < inventory.length; i++) {
+        inventory_blocks = inventory_blocks.concat(`
+          <label class="panel-block">
+            <input type="checkbox">
+            ${inventory[i]}
+          </label>
+        `)
+      }
+      return inventory_blocks;
+    })()}
   </nav>
   `;
 }
