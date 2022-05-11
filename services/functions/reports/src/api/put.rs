eda6add9f4e9fd7252d809c0b798e4323e8d3b50
Everett Pompeii <epompeii@protonmail.com>
2022-05-11 06:57:19 -0400

    prod docker update

--- services/functions/reports/src/api/put.rs
+++ services/functions/reports/src/api/put.rs
@@ -5,6 +5,7 @@ use dropshot::HttpError;
 use dropshot::HttpResponseAccepted;
 use dropshot::RequestContext;
 use dropshot::TypedBody;
+use email_address_parser::EmailAddress;
 
 use reports::Report;
 
@@ -15,7 +16,15 @@ use reports::Report;
 pub async fn api_put_reports(
     _rqctx: Arc<RequestContext<()>>,
     body: TypedBody<Report>,
-) -> Result<HttpResponseAccepted<Report>, HttpError> {
-    let body = body.into_inner();
-    Ok(HttpResponseAccepted(body))
+) -> Result<HttpResponseAccepted<()>, HttpError> {
+    let report = body.into_inner();
+    let email = map_email(report.email)?;
+    Ok(HttpResponseAccepted(()))
+}
+
+fn map_email(email: String) -> Result<EmailAddress, HttpError> {
+    EmailAddress::parse(&email, None).ok_or(HttpError::for_bad_request(
+        Some(String::from("BadInput")),
+        format!("Failed to parse email: {email}"),
+    ))
 }
