660c65a891144723d4a7ef43ec995fe0ce3bab7a
Everett Pompeii <epompeii@protonmail.com>
2022-05-11 22:01:49 -0400

    write to db

--- services/api/reports/src/api/put.rs
+++ services/api/reports/src/api/put.rs
@@ -1,7 +1,11 @@
 use std::sync::Arc;
 use std::sync::Mutex;
 
+use chrono::DateTime;
+use chrono::Utc;
 use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use diesel::RunQueryDsl;
 use dropshot::endpoint;
 use dropshot::HttpError;
 use dropshot::HttpResponseAccepted;
@@ -9,18 +13,39 @@ use dropshot::RequestContext;
 use dropshot::TypedBody;
 use email_address_parser::EmailAddress;
 
-use reports::Report;
+use reports::Report as ReportJson;
+
+use util::db::model::{NewReport, Report};
+use util::db::schema::report;
 
 #[endpoint {
     method = PUT,
     path = "/v0/reports",
 }]
 pub async fn api_put_reports(
-    _rqctx: Arc<RequestContext<Mutex<PgConnection>>>,
-    body: TypedBody<Report>,
+    rqctx: Arc<RequestContext<Mutex<PgConnection>>>,
+    body: TypedBody<ReportJson>,
 ) -> Result<HttpResponseAccepted<()>, HttpError> {
+    let db_connection = rqctx.context();
+
     let report = body.into_inner();
     let email = map_email(report.email)?;
+
+    let new_report = NewReport {
+        date_time: Utc::now(),
+        hash: 55,
+        length: 55,
+    };
+
+    if let Ok(db_conn) = db_connection.lock() {
+        let db_conn = &*db_conn;
+        let report: Report = diesel::insert_into(report::table)
+            .values(&new_report)
+            .get_result(db_conn)
+            .expect("Error saving new post");
+        println!("{report:?}")
+    }
+
     Ok(HttpResponseAccepted(()))
 }
 
--- services/api/util/src/db/model/mod.rs
+++ services/api/util/src/db/model/mod.rs
@@ -1,11 +1,22 @@
 use chrono::DateTime;
 use chrono::Utc;
+use diesel::Insertable;
 use diesel::Queryable;
 
-#[derive(Queryable)]
+use crate::db::schema::report;
+
+#[derive(Queryable, Debug)]
 pub struct Report {
-    pub id: u64,
+    pub id: i32,
     pub date_time: DateTime<Utc>,
-    pub hash: u64,
-    pub length: u64,
+    pub hash: i32,
+    pub length: i32,
+}
+
+#[derive(Insertable)]
+#[table_name = "report"]
+pub struct NewReport {
+    pub date_time: DateTime<Utc>,
+    pub hash: i32,
+    pub length: i32,
 }
