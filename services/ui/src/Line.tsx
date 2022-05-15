f68830357b0402c5d672441c8137e62b9ebd125c
Everett Pompeii <epompeii@protonmail.com>
2022-05-15 14:20:17 -0400

    multi-line

--- services/ui/src/Line.tsx
+++ services/ui/src/Line.tsx
@@ -1,57 +1,9 @@
 import { createSignal, createEffect } from "solid-js";
 
-const [metrics, setMetrics] = createSignal(null);
+const [metaMetrics, setMetaMetrics] = createSignal([]);
 
 import * as Plot from "@observablehq/plot";
 import axios from "axios";
-
-const aapl = [
-  {
-    Date: "2013-05-13T00:00:00.000Z",
-    Open: 64.501427,
-    High: 65.414284,
-    Low: 64.5,
-    Close: 64.96286,
-    "Adj Close": 50.961628,
-    Volume: 79237200,
-  },
-  {
-    Date: "2013-05-14T00:00:00.000Z",
-    Open: 64.835716,
-    High: 65.028572,
-    Low: 63.164288,
-    Close: 63.408573,
-    "Adj Close": 49.742329,
-    Volume: 111779500,
-  },
-  {
-    Date: "2013-05-15T00:00:00.000Z",
-    Open: 62.737144,
-    High: 63,
-    Low: 60.337143,
-    Close: 61.264286,
-    "Adj Close": 48.060188,
-    Volume: 185403400,
-  },
-  {
-    Date: "2013-05-16T00:00:00.000Z",
-    Open: 60.462856,
-    High: 62.549999,
-    Low: 59.842857,
-    Close: 62.082859,
-    "Adj Close": 48.702328,
-    Volume: 150801000,
-  },
-  {
-    Date: "2013-05-17T00:00:00.000Z",
-    Open: 62.721428,
-    High: 62.869999,
-    Low: 61.572857,
-    Close: 61.894287,
-    "Adj Close": 48.554409,
-    Volume: 106976100,
-  },
-];
 
 const BENCHER_API_URL: String = "http://localhost"; 
 
@@ -65,39 +17,58 @@ const options = {
   }
 };
 
-async function fetchMetrics() {
+async function fetchMetaMetrics() {
   try {
     const response = await axios(options);
     console.log(response?.data);
-    setMetrics(response?.data);
+    setMetaMetrics(response?.data);
   } catch (error) {
     console.error(error);
   }
 }
 
-await fetchMetrics();
+await fetchMetaMetrics();
 
+const intoDataArrays = () => {
+  const meta_metrics_array = metaMetrics();
+  var data_arrays = {};
+  for (let i = 0; i < meta_metrics_array.length; i++) {
+    let meta_metrics_data = meta_metrics_array[i];
 
-const intoData = () => {
-  const metrics_array = metrics();
-  var data_array = [];
-  for (let i = 0; i < metrics_array.length; i++) {
-    let metrics_data = metrics_array[i];
-    let date_time = metrics_data.date_time;
-    let latency = metrics_data?.metrics["tests::benchmark_a"]?.latency?.duration;
-    let nanos = latency?.secs * 1_000_000_000 + latency?.nanos;
-    data_array.push([date_time, nanos]);
+    let date_time = meta_metrics_data.date_time;
+    let metrics_obj = meta_metrics_data?.metrics;
+
+    for (const [key, value] of Object.entries(metrics_obj)) {
+      let latency = value?.latency?.duration;
+      let nanos = latency?.secs * 1_000_000_000 + latency?.nanos;
+      let xy = [date_time, nanos];
+      if (data_arrays[key] === undefined) {
+        data_arrays[key] = [xy];
+      } else {
+        data_arrays[key].push(xy);
+      }
+    }
   }
-  return data_array;
+  return data_arrays;
 }
 
-const plot_data = intoData();
+const intoPlotMarks = () => {
+  let data_arrays = intoDataArrays();  
+  let plot_arrays = [];
+
+  for (const [key, value] of Object.entries(data_arrays)) {
+    plot_arrays.push(Plot.line(value));
+  }
+  return plot_arrays;
+}
+
+const plot_marks = intoPlotMarks();
 
 const plot = Plot.plot({
   y: {
     grid: true,
   },
-  marks: [Plot.line(plot_data)],
+  marks: plot_marks,
 });
 
 export function LinePlot(prop) {
