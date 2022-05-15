95b475be494a8866eb4305cf013f408a1ddded76
Everett Pompeii <epompeii@protonmail.com>
2022-05-15 15:30:54 -0400

    navbar

--- services/ui/src/App.tsx
+++ services/ui/src/App.tsx
@@ -2,18 +2,22 @@ import "./styles/styles.scss";
 import { Component } from "solid-js";
 
 import { LinePlot } from "./components/LinePlot";
+import { Navbar } from "./components/site/Navbar";
 
 const App: Component = () => {
   return (
-    <section class="section">
-      <div class="container">
-        <div class="columns">
-          <div class="column">
-            <LinePlot />
+    <>
+      <Navbar />
+      <section class="section">
+        <div class="container">
+          <div class="columns">
+            <div class="column">
+              <LinePlot />
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
---
+++ services/ui/src/components/site/Navbar.tsx
@@ -0,0 +1,64 @@
+export function Navbar() {
+    return (
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://bulma.io">
+            <img src="/bencher_navbar.png" width="152" height="28"/>
+          </a>
+      
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+      
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item">
+              Home
+            </a>
+      
+            <a class="navbar-item">
+              Documentation
+            </a>
+      
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                More
+              </a>
+      
+              <div class="navbar-dropdown">
+                <a class="navbar-item">
+                  About
+                </a>
+                <a class="navbar-item">
+                  Jobs
+                </a>
+                <a class="navbar-item">
+                  Contact
+                </a>
+                <hr class="navbar-divider"/>
+                <a class="navbar-item">
+                  Report an issue
+                </a>
+              </div>
+            </div>
+          </div>
+      
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a class="button is-light">
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
