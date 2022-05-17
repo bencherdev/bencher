8d5722a3e0640df57860d098998d0a99c769baf5
Everett Pompeii <epompeii@protonmail.com>
2022-05-16 22:03:16 -0400

    page_view

--- services/ui/src/App.tsx
+++ services/ui/src/App.tsx
@@ -1,20 +1,40 @@
 import "./styles/styles.scss";
 import { Component } from "solid-js";
 
-// import Analytics from 'analytics'
-// import googleAnalytics from '@analytics/google-analytics'
-
-// const analytics = Analytics({
-//   app: "bencher.dev",
-//   plugins: [
-//     googleAnalytics({
-//       trackingId: import.meta.env.VITE_GOOGLE_ANALYTICS
-//     })
-//   ]
-// })
+import { Analytics, AnalyticsInstance} from 'analytics'
+import googleAnalytics from '@analytics/google-analytics'
 
 import { LinePlot } from "./components/LinePlot";
 import { Navbar } from "./components/site/Navbar";
+
+
+
+const get_analytics: () => AnalyticsInstance | null = () => {
+  let google_analytics = import.meta.env.VITE_GOOGLE_ANALYTICS;
+  if (google_analytics === undefined) {
+    return;
+  } else {
+    return Analytics({
+      app: "bencher.dev",
+      plugins: [
+        googleAnalytics({
+          trackingId: google_analytics,
+        })
+      ]
+    });
+  }
+}
+
+const count_page_view = (analytics: AnalyticsInstance | null) => {
+  if (analytics === undefined) {
+    return;
+  } else {
+    analytics?.id.page()
+  }
+}
+
+const analytics = get_analytics();
+count_page_view(analytics);
 
 const App: Component = () => {
   return (
