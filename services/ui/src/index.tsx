a8671ab22fc428268cfcf92bf6579a1c23ab554d
Everett Pompeii <epompeii@protonmail.com>
2022-06-23 08:57:51 -0400

    router

--- services/ui/src/index.tsx
+++ services/ui/src/index.tsx
@@ -1,6 +1,14 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
+import { Router } from "solid-app-router";
 
 import App from "./App";
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+render(
+  () => (
+    <Router>
+      <App />
+    </Router>
+  ),
+  document.getElementById("root") as HTMLElement
+);
