01ee32912ea05317217905f1902500e004b48c2c
Everett Pompeii <epompeii@protonmail.com>
2022-07-08 19:51:14 -0400

    exec

--- services/api/entrypoint.sh
+++ services/api/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+set -e
 
-litestream restore -o $LITESTREAM_DB_PATH $LITESTREAM_REPLICA_URL
-nohup litestream replicate &
-./api
+# Restore the database if it does not already exist.
+if [ -f /bencher.db ]; then
+	echo "Database already exists, skipping restore."
+else
+	echo "No database found, restoring from replica if it exists."
+	litestream restore -v -if-replica-exists -o "$LITESTREAM_DB_PATH" "$LITESTREAM_REPLICA_URL"
+fi
+
+# Run litestream with your app as the subprocess.
+exec litestream replicate -exec "/api"
