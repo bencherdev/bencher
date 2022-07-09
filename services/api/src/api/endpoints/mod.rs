e55136140db04567e4e2447ad99e85d4001af8e4
Everett Pompeii <epompeii@protonmail.com>
2022-07-09 11:29:18 -0400

    endpoints

---
+++ services/api/src/api/endpoints/mod.rs
@@ -0,0 +1,3 @@
+pub mod adapters;
+pub mod ping;
+pub mod reports;
--- services/api/src/api/mod.rs
+++ services/api/src/api/mod.rs
@@ -2,11 +2,9 @@ use diesel::sqlite::SqliteConnection;
 use dropshot::ApiDescription;
 use tokio::sync::Mutex;
 
-pub mod adapters;
+pub mod endpoints;
 pub mod headers;
-pub mod ping;
 pub mod registrar;
-pub mod reports;
 pub mod server;
 
 use registrar::Registrar;
@@ -15,12 +13,12 @@ pub struct Api;
 
 impl Registrar<Mutex<SqliteConnection>> for Api {
     fn register(&self, api: &mut ApiDescription<Mutex<SqliteConnection>>) -> Result<(), String> {
-        api.register(ping::api_get_ping)?;
-        api.register(adapters::api_get_adapters)?;
-        api.register(adapters::api_get_adapter)?;
-        api.register(reports::api_get_reports)?;
-        api.register(reports::api_get_report)?;
-        api.register(reports::api_post_report)?;
+        api.register(endpoints::ping::api_get_ping)?;
+        api.register(endpoints::adapters::api_get_adapters)?;
+        api.register(endpoints::adapters::api_get_adapter)?;
+        api.register(endpoints::reports::api_get_reports)?;
+        api.register(endpoints::reports::api_get_report)?;
+        api.register(endpoints::reports::api_post_report)?;
         Ok(())
     }
 }
