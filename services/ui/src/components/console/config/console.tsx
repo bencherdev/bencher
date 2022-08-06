e9b71756e319626623111605048eacef2e2a1c03
Everett Pompeii <epompeii@protonmail.com>
2022-08-06 10:12:20 -0400

    ui

--- services/ui/src/components/console/config/console.tsx
+++ services/ui/src/components/console/config/console.tsx
@@ -2,9 +2,11 @@ import { Resource } from "./types";
 import projectsConfig from "./projects";
 import testbedsConfig from "./testbeds";
 import branchesConfig from "./branches";
+import reportsConfig from "./reports";
 
 const consoleConfig = {
   [Resource.PROJECTS]: projectsConfig,
+  [Resource.REPORTS]: reportsConfig,
   [Resource.BRANCHES]: branchesConfig,
   [Resource.TESTBEDS]: testbedsConfig,
 };
---
+++ services/ui/src/components/console/config/reports.tsx
@@ -0,0 +1,107 @@
+import projectFieldsConfig from "../../fields/config/project/projectFieldsConfig";
+import branchFieldsConfig from "../../fields/config/project/branchFieldsConfig";
+import { Button, Card, Field, Operation, Row } from "./types";
+import { BENCHER_API_URL, parentPath, addPath, viewPath } from "./util";
+
+const reportsConfig = {
+  [Operation.LIST]: {
+    operation: Operation.LIST,
+    header: {
+      title: "Reports",
+      buttons: [
+        {
+          kind: Button.ADD,
+          path: (pathname) => {
+            return addPath(pathname);
+          },
+        },
+        { kind: Button.REFRESH },
+      ],
+    },
+    table: {
+      url: (path_params) => {
+        return `${BENCHER_API_URL}/v0/projects/${path_params?.project_slug}/reports`;
+      },
+      row: {
+        key: "start_time",
+        items: [{}, {}, {}, {}],
+        path: (pathname, datum) => {
+          return viewPath(pathname, datum);
+        },
+      },
+    },
+  },
+  [Operation.ADD]: {
+    operation: Operation.ADD,
+    header: {
+      title: "Add Branch",
+      path: (pathname) => {
+        return parentPath(pathname);
+      },
+    },
+    form: {
+      url: `${BENCHER_API_URL}/v0/branches`,
+      fields: [
+        {
+          kind: Field.FIXED,
+          key: "project",
+          label: true,
+          value: "",
+          valid: null,
+          validate: true,
+          nullify: false,
+          clear: false,
+          config: projectFieldsConfig.slug,
+        },
+        {
+          kind: Field.INPUT,
+          key: "name",
+          label: true,
+          value: "",
+          valid: null,
+          validate: true,
+          nullify: false,
+          clear: false,
+          config: branchFieldsConfig.name,
+        },
+      ],
+      path: (pathname) => {
+        return parentPath(pathname);
+      },
+    },
+  },
+  [Operation.VIEW]: {
+    operation: Operation.VIEW,
+    header: {
+      key: "name",
+      path: (pathname) => {
+        return parentPath(pathname);
+      },
+    },
+    deck: {
+      url: (path_params) => {
+        return `${BENCHER_API_URL}/v0/projects/${path_params?.project_slug}/branches/${path_params?.branch_slug}`;
+      },
+      cards: [
+        {
+          kind: Card.FIELD,
+          field: "Branch Name",
+          key: "name",
+        },
+        {
+          kind: Card.FIELD,
+          field: "Branch Slug",
+          key: "slug",
+        },
+        // {
+        //   kind: Card.TABLE,
+        //   field: "Versions",
+        //   key: "versions",
+        // },
+      ],
+      buttons: false,
+    },
+  },
+};
+
+export default reportsConfig;
