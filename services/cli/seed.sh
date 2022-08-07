b660ebbc64b79074cf70ea041d6786b5ab2e166b
Everett Pompeii <epompeii@protonmail.com>
2022-08-07 14:10:41 -0400

    host

--- services/cli/seed.sh
+++ services/cli/seed.sh
@@ -1,22 +1,24 @@
 # run as `source ./seed.sh` or `. ./seed.sh`
 cargo test --features seed --test seed
 
-LOGIN=$(cargo run -- auth login --host http://localhost:8080 muriel.bagge@nowhere.com)
+export BENCHER_HOST=http://localhost:8080
+
+LOGIN=$(cargo run -- auth login muriel.bagge@nowhere.com)
 LOGIN_UUID=$(echo "$LOGIN" | sed -n -e 's/^.*"uuid": //p')
 LOGIN_UUID=$(echo "$LOGIN_UUID" | tr -d '"')
 echo $LOGIN_UUID
 export BENCHER_TOKEN=$LOGIN_UUID
 
-BRANCH=$(cargo run -- branch view --host http://localhost:8080 --project the-computer master)
+BRANCH=$(cargo run -- branch view --project the-computer master)
 BRANCH_UUID=$(echo "$BRANCH" | sed -n -e 's/^.*"uuid": //p')
 BRANCH_UUID=$(echo "$BRANCH_UUID" | tr -d '"')
 echo $BRANCH_UUID
 export BENCHER_BRANCH=$BRANCH_UUID
 
-TESTBED=$(cargo run -- testbed view --host http://localhost:8080 --project the-computer base)
+TESTBED=$(cargo run -- testbed view --project the-computer base)
 TESTBED_UUID=$(echo "$TESTBED" | sed -n -e 's/^.*"uuid": //p')
 TESTBED_UUID=$(echo "$TESTBED_UUID" | tr -d '"')
 echo $TESTBED_UUID
 export BENCHER_TESTBED=$TESTBED_UUID
 
-cargo run -- run --host http://localhost:8080 --adapter rust "cargo bench"
+cargo run -- run --adapter rust "cargo bench"
