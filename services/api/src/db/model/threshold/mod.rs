e6070a6191150e9e7797deaeafa727d4e13262fd
Everett Pompeii <epompeii@protonmail.com>
2022-08-21 10:53:47 -0400

    InsertThreshold

--- services/api/src/db/model/threshold/mod.rs
+++ services/api/src/db/model/threshold/mod.rs
@@ -1,2 +1,38 @@
-pub mod t_test;
-pub mod z_score;
+use bencher_json::threshold::JsonNewThreshold;
+use diesel::{
+    Insertable,
+    SqliteConnection,
+};
+use dropshot::HttpError;
+use uuid::Uuid;
+
+use super::{
+    branch::QueryBranch,
+    testbed::QueryTestbed,
+};
+use crate::db::schema::threshold as threshold_table;
+
+mod t_test;
+mod z_score;
+
+#[derive(Insertable)]
+#[table_name = "threshold_table"]
+pub struct InsertThreshold {
+    pub uuid:       String,
+    pub branch_id:  i32,
+    pub testbed_id: i32,
+}
+
+impl InsertThreshold {
+    pub fn from_json(
+        conn: &SqliteConnection,
+        json_threshold: JsonNewThreshold,
+    ) -> Result<Self, HttpError> {
+        let JsonNewThreshold { branch, testbed } = json_threshold;
+        Ok(Self {
+            uuid:       Uuid::new_v4().to_string(),
+            branch_id:  QueryBranch::get_id(conn, &branch)?,
+            testbed_id: QueryTestbed::get_id(conn, &testbed)?,
+        })
+    }
+}
