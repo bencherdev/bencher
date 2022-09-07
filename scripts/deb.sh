2ef6e77da1c9c77fa375b701f333c4b7453c164f
Everett Pompeii <epompeii@protonmail.com>
2022-09-07 10:04:08 -0400

    deb

---
+++ scripts/deb.sh
@@ -0,0 +1,24 @@
+VERSION=$1
+ARCH=amd64
+TRIPLE=bencher_${VERSION}_${ARCH}
+DEB_PATH=deb/$TRIPLE
+
+cd ../services/cli
+cargo build --release --target x86_64-unknown-linux-gnu
+
+cd ../..
+BIN_PATH=$DEB_PATH/usr/local/bin
+mkdir -p $BIN_PATH
+cp target/release/bencher $BIN_PATH
+
+DEBIAN_PATH=$DEB_PATH/DEBIAN
+mkdir $DEBIAN_PATH
+echo \
+"Package: bencher
+Version: $VERSION
+Architecture: $ARCH
+Maintainer: Bencher <info@bencher.dev>
+Description: Track your benchmarks. Catch performance regressions in CI." \
+> $DEBIAN_PATH/control
+
+dpkg-deb --build --root-owner-group $DEB_PATH
