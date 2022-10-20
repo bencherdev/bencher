4adbf2c0280cc6d0dba1060788a13a67175f4f26
Everett Pompeii <epompeii@protonmail.com>
2022-10-20 06:55:45 -0400

    json_config

--- services/lib/bencher_json/src/config/mod.rs
+++ services/lib/bencher_json/src/config/mod.rs
@@ -1,7 +1,28 @@
 #[cfg(feature = "schema")]
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use url::Url;
 
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[cfg_attr(feature = "schema", derive(JsonSchema))]
-pub struct JsonConfig {}
+pub struct JsonConfig {
+    #[serde(default = "default_url")]
+    url: Url,
+}
+
+fn default_url() -> Url {
+    let default = {
+        #[cfg(debug_assertions)]
+        {
+            "http://localhost:3000"
+        }
+        #[cfg(not(debug_assertions))]
+        {
+            "https://bencher.dev"
+        }
+    };
+
+    default
+        .parse()
+        .expect(&format!("Invalid default URL: {default}"))
+}
