2867cd4fda2daf345c02ba3b4efe15623c5728d6
Everett Pompeii <epompeii@protonmail.com>
2022-10-20 07:03:10 -0400

    config

--- services/lib/bencher_json/src/config/mod.rs
+++ services/lib/bencher_json/src/config/mod.rs
@@ -1,28 +1,40 @@
 #[cfg(feature = "schema")]
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-use url::Url;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[cfg_attr(feature = "schema", derive(JsonSchema))]
 pub struct JsonConfig {
     #[serde(default = "default_url")]
-    url: Url,
+    pub url: String,
+    pub database: JsonDatabase,
+    pub smtp: Option<JsonSmtp>,
 }
 
-fn default_url() -> Url {
-    let default = {
-        #[cfg(debug_assertions)]
-        {
-            "http://localhost:3000"
-        }
-        #[cfg(not(debug_assertions))]
-        {
-            "https://bencher.dev"
-        }
-    };
+fn default_url() -> String {
+    #[cfg(debug_assertions)]
+    {
+        "http://localhost:3000".into()
+    }
+    #[cfg(not(debug_assertions))]
+    {
+        "https://bencher.dev".into()
+    }
+}
 
-    default
-        .parse()
-        .expect(&format!("Invalid default URL: {default}"))
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[cfg_attr(feature = "schema", derive(JsonSchema))]
+pub struct JsonDatabase {
+    pub path: String,
+    pub name: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[cfg_attr(feature = "schema", derive(JsonSchema))]
+pub struct JsonSmtp {
+    pub hostname: String,
+    pub username: String,
+    pub secret: String,
+    pub from_name: String,
+    pub from_email: String,
 }
