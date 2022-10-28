abda875f58336a5ae6b8c3d99480dbc4e28b3b1a
Everett Pompeii <epompeii@protonmail.com>
2022-10-28 00:49:54 +0000

    test

--- tests/cli/rust_bench/test.sh
+++ tests/cli/rust_bench/test.sh
@@ -7,6 +7,7 @@ export BENCHER_HOST=http://localhost:61016
 
 # Valid until 2027-09-05T19:03:59Z
 export BENCHER_API_TOKEN=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhcGlfa2V5IiwiZXhwIjoxODIwMTcxMDM5LCJpYXQiOjE2NjIzODY0MDksImlzcyI6ImJlbmNoZXIuZGV2Iiwic3ViIjoibXVyaWVsLmJhZ2dlQG5vd2hlcmUuY29tIn0.sfAJmF9qIl_QRNnh8uLYuODHnxufXt_3m7skcNp1kMs
+export BENCHER_PROJECT=the-computer
 
 BRANCH=$(cargo run -- branch view --project the-computer master)
 BRANCH_UUID=$(echo "$BRANCH" | sed -n -e 's/^.*"uuid": //p')
