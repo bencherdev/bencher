0b12ad4aa679882b52a0cf8261a7305ca17feb5d
Everett Pompeii <epompeii@protonmail.com>
2022-10-29 11:49:46 +0000

    endpoint

--- services/lib/bencher_endpoint/src/lib.rs
+++ services/lib/bencher_endpoint/src/lib.rs
@@ -14,4 +14,12 @@ pub type Organizations = PathParam<Organization, OrganizationResource>;
 
 pub struct Organization;
 
-pub enum OrganizationResource {}
+pub enum OrganizationResource {
+    Members(Option<Members>),
+}
+
+pub type Members = PathParam<Member, MemberResource>;
+
+pub struct Member;
+
+pub enum MemberResource {}
