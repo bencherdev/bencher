dc679556eb0ad66fd431bdf1c8abf0623606a297
Everett Pompeii <epompeii@protonmail.com>
2022-10-30 15:59:20 +0000

    api_docs

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Generate the API docs from the server
+cd ./services/api
+cargo run --features swagger
+cd -
+
+# If there was a change/the git tree is dirty add the updated file and commit
+git diff --quiet || (git add ./ui/src/components/docs/api/swagger.json && git commit -m "Update Swagger")
+
 TAG=$(./scripts/version.sh)
 git tag $TAG
 git push origin $TAG
