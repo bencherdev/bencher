cbd5ba0ceff09c9027b9b1d441ffa418cffe294e
Everett Pompeii <epompeii@protonmail.com>
2022-10-30 16:27:06 +0000

    Release 0.2.4

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-TAG=$(./scripts/version.sh)
-
 # Generate the API docs from the server
 cd ./services/api
 cargo run --features swagger
 cd -
 
 # If there was a change/the git tree is dirty add the updated file and commit
-git diff --quiet || (git add ./services/ui/src/components/docs/api/swagger.json && git commit -m "Update Swagger for $TAG")
+SWAGGER=./services/ui/src/components/docs/api/swagger.json
+git diff --quiet $SWAGGER || git add $SWAGGER
 
+TAG=$(./scripts/version.sh)
+COMMIT="Release $TAG"
+echo $COMMIT
+git commit -m "$COMMIT"
 git tag $TAG
 git push origin $TAG
