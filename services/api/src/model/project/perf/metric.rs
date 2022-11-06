e095cc9f64bf9047610f899ebc17c96b11472366
Everett Pompeii <epompeii@protonmail.com>
2022-11-06 21:03:15 +0000

    hold

--- services/api/src/model/project/perf/metric.rs
+++ services/api/src/model/project/perf/metric.rs
@@ -9,6 +9,8 @@ use crate::{schema, schema::metric as metric_table, util::map_http_error};
 pub struct QueryMetric {
     pub id: i32,
     pub uuid: String,
+    pub perf_id: i32,
+    pub metric_kind_id: i32,
     pub value: f64,
     pub lower_bound: Option<f64>,
     pub upper_bound: Option<f64>,
@@ -19,6 +21,8 @@ impl QueryMetric {
         let Self {
             id: _,
             uuid: _,
+            perf_id: _,
+            metric_kind_id: _,
             value,
             lower_bound,
             upper_bound,
@@ -35,34 +39,38 @@ impl QueryMetric {
 #[diesel(table_name = metric_table)]
 pub struct InsertMetric {
     pub uuid: String,
+    pub perf_id: i32,
+    pub metric_kind_id: i32,
     pub value: f64,
     pub lower_bound: Option<f64>,
     pub upper_bound: Option<f64>,
 }
 
-impl From<JsonMetric> for InsertMetric {
-    fn from(latency: JsonMetric) -> Self {
+impl InsertMetric {
+    fn from_json(perf_id: i32, metric_kind_id: i32, metric: JsonMetric) -> Self {
         let JsonMetric {
             value,
             lower_bound,
             upper_bound,
-        } = latency;
+        } = metric;
         Self {
+            perf_id,
+            metric_kind_id,
             uuid: Uuid::new_v4().to_string(),
             value: value.into(),
             lower_bound: lower_bound.map(Into::into),
             upper_bound: upper_bound.map(Into::into),
         }
     }
-}
 
-impl InsertMetric {
     pub fn map_json(
         conn: &mut SqliteConnection,
+        perf_id: i32,
+        metric_kind_id: i32,
         metric: Option<JsonMetric>,
     ) -> Result<Option<i32>, HttpError> {
         Ok(if let Some(json_metric) = metric {
-            let insert_metric: InsertMetric = json_metric.into();
+            let insert_metric: InsertMetric = Self::from_json(perf_id, metric_kind_id, metric);
             diesel::insert_into(schema::metric::table)
                 .values(&insert_metric)
                 .execute(conn)
