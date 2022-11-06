90dc06290e1464dac2dd58ffedaa06b2c5441c0d
Everett Pompeii <epompeii@protonmail.com>
2022-11-06 16:38:52 +0000

    diesel

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,27 +2,14 @@
 
 ./scripts/githooks.sh
 
-curl -L https://fly.io/install.sh | sh
-echo "export FLYCTL_INSTALL=\"/home/gitpod/.fly\"" >> $HOME/.bash_profile
-echo "export PATH=\"/home/gitpod/.fly/bin:$PATH\"" >> $HOME/.bash_profile
+sudo apt-get update -q
+sudo apt-get install -yq netcat-openbsd sqlite3
 
 rustup toolchain install nightly
 rustup component add rustfmt
 rustup component add clippy
 cargo install cargo-udeps --locked
-
-sudo apt-get update -q
-sudo apt-get install -yq netcat-openbsd
-
-echo "Waiting for vscode-sqltools"
-until [ -d ~/.local/share/vscode-sqltools ]
-do
-    sleep .1
-done
-
-cd ~/.local/share/vscode-sqltools
-npm install sqlite3
-cd -
+cargo install diesel_cli --no-default-features --features sqlite --locked
 
 echo "Waiting for API server"
 while ! nc -z localhost 61016; do
@@ -32,6 +19,10 @@ done
 cd ./tests/cli/rust_bench
 ./test.sh
 cd -
+
+curl -L https://fly.io/install.sh | sh
+echo "export FLYCTL_INSTALL=\"/home/gitpod/.fly\"" >> $HOME/.bash_profile
+echo "export PATH=\"/home/gitpod/.fly/bin:$PATH\"" >> $HOME/.bash_profile
 
 source ~/.bash_profile
 flyctl auth login
