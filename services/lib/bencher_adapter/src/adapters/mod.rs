8ec9788070786584944aee617f7d29ecd3786dbd
Everett Pompeii <epompeii@protonmail.com>
2022-11-08 19:26:55 +0000

    adapter

---
+++ services/lib/bencher_adapter/src/adapters/mod.rs
@@ -0,0 +1,23 @@
+use bencher_json::project::report::JsonAdapter;
+
+pub mod json;
+pub mod rust;
+
+#[derive(Clone, Copy, Debug, Default)]
+pub enum Adapter {
+    #[default]
+    Json,
+    RustTest,
+    RustBench,
+    // RustCriterion,
+}
+
+impl From<Adapter> for JsonAdapter {
+    fn from(adapter: Adapter) -> Self {
+        match adapter {
+            Adapter::Json => Self::Json,
+            Adapter::RustTest => Self::RustTest,
+            Adapter::RustBench => Self::RustBench,
+        }
+    }
+}
