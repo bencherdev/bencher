879c0cc827b23e32a340a16e723d6b267cc7eac1
Everett Pompeii <epompeii@protonmail.com>
2022-11-12 03:43:29 +0000

    mdx

--- services/ui/vite.config.ts
+++ services/ui/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
+import solid from "vite-plugin-solid";
+import mdx from "@mdx-js/rollup";
+import remarkGfm from "remark-gfm";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [mdx({ jsxImportSource: "solid-jsx", remarkPlugins: [remarkGfm] }), solid()],
   build: {
     target: "esnext",
   },
