9a13c09b14a6bb440f030b84c7e4a920da9122c2
Everett Pompeii <epompeii@protonmail.com>
2022-11-19 23:47:04 +0000

    bundle

--- services/ui/src/components/docs/api/SwaggerPanel.tsx
+++ services/ui/src/components/docs/api/SwaggerPanel.tsx
@@ -1,17 +1,37 @@
 import { createEffect } from "solid-js";
 import SwaggerUI from "swagger-ui";
 import swagger from "./swagger.json";
+import { SwaggerUIBundle } from "swagger-ui-dist";
 
 const SwaggerPanel = (props) => {
   createEffect(() => {
-    SwaggerUI({
-      dom_id: "#swagger",
-      spec: swagger,
-      layout: "BaseLayout",
-    });
+    // const x = SwaggerUI({
+    //   // dom_id: "#swagger",
+    //   spec: swagger,
+    //   layout: "BaseLayout",
+    // });
+    // console.log(x);
+    // console.log(
+    //   SwaggerUI({
+    //     // dom_id: "#swagger",
+    //     spec: swagger,
+    //     layout: "BaseLayout",
+    //   }).getComponents()
+    // );
+    console.log(
+      SwaggerUIBundle({
+        dom_id: "#swagger",
+        spec: swagger,
+        layout: "BaseLayout",
+      })
+    );
   });
 
-  return <div id="swagger" />;
+  return (
+    <div>
+      <div id="swagger" />
+    </div>
+  );
 };
 
 export default SwaggerPanel;
