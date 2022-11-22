816012422db6e98cb7ef7c86350db13d908493d6
Everett Pompeii <epompeii@protonmail.com>
2022-11-22 01:30:41 +0000

    init

---
+++ scripts/init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+./scripts/githooks.sh
+
+sudo apt-get update -q
+sudo apt-get install -yq netcat-openbsd sqlite3
+
+rustup toolchain install nightly
+rustup component add rust-src
+rustup component add rustfmt
+rustup component add clippy
+cargo install cargo-udeps --locked
+cargo install diesel_cli --no-default-features --features sqlite --locked
+
+rustup target add wasm32-unknown-unknown
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
+
+curl -L https://fly.io/install.sh | sh
+echo "export FLYCTL_INSTALL=\"/home/gitpod/.fly\"" >> $HOME/.bash_profile
+echo "export PATH=\"/home/gitpod/.fly/bin:$PATH\"" >> $HOME/.bash_profile
+
+source ~/.bash_profile
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-./scripts/githooks.sh
-
-sudo apt-get update -q
-sudo apt-get install -yq netcat-openbsd sqlite3
-
-rustup toolchain install nightly
-rustup comonent add rust-src
-rustup component add rustfmt
-rustup component add clippy
-cargo install cargo-udeps --locked
-cargo install diesel_cli --no-default-features --features sqlite --locked
-
-rustup target add wasm32-unknown-unknown
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
-
 echo "Waiting for API server"
 while ! nc -z localhost 61016; do
   sleep .1
@@ -23,10 +8,3 @@ done
 cd ./tests/cli/rust_bench
 ./test.sh
 cd -
-
-curl -L https://fly.io/install.sh | sh
-echo "export FLYCTL_INSTALL=\"/home/gitpod/.fly\"" >> $HOME/.bash_profile
-echo "export PATH=\"/home/gitpod/.fly/bin:$PATH\"" >> $HOME/.bash_profile
-
-source ~/.bash_profile
-flyctl auth login
