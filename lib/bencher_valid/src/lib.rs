6745b51761b8c415389c9866b74c79e27f9cb423
Everett Pompeii <epompeii@protonmail.com>
2022-11-22 02:01:43 +0000

    parse_email

--- lib/bencher_valid/src/lib.rs
+++ lib/bencher_valid/src/lib.rs
@@ -1,16 +1,24 @@
-mod utils;
-
+use email_address_parser::EmailAddress;
 #[cfg(feature = "wasm")]
 use wasm_bindgen::prelude::*;
 
-#[cfg_attr(feature = "wasm", wasm_bindgen(start))]
+#[cfg(feature = "wasm")]
+mod utils;
+
+#[cfg(feature = "wasm")]
+#[wasm_bindgen(start)]
 pub fn startup() {
     utils::set_panic_hook();
-    console_log::init_with_level(log::Level::Debug).expect("Error init console log");
+
+    #[cfg(debug_assertions)]
+    let log_level = log::Level::Debug;
+    #[cfg(not(debug_assertions))]
+    let log_level = log::Level::Info;
+    console_log::init_with_level(log_level).expect("Error init console log");
     log::debug!("Bencher Validation");
 }
 
 #[cfg_attr(feature = "wasm", wasm_bindgen)]
 pub fn validate_email(email: &str) -> bool {
-    true
+    EmailAddress::parse(email, None).is_some()
 }
