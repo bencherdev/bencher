40b9f758f8ace35dbb97abb3a2ce68ebbf2f7e20
Everett Pompeii <epompeii@protonmail.com>
2022-11-22 01:48:43 +0000

    wasm

--- lib/bencher_valid/src/lib.rs
+++ lib/bencher_valid/src/lib.rs
@@ -1,13 +1,16 @@
 mod utils;
 
+#[cfg(feature = "wasm")]
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+#[cfg_attr(feature = "wasm", wasm_bindgen(start))]
+pub fn startup() {
+    utils::set_panic_hook();
+    console_log::init_with_level(log::Level::Debug).expect("Error init console log");
+    log::debug!("Bencher Validation");
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, bencher-valid!");
+#[cfg_attr(feature = "wasm", wasm_bindgen)]
+pub fn validate_email(email: &str) -> bool {
+    true
 }
