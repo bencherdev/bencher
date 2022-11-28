6ba1ff09960b9481ed6d9ab90ab197fb1a7d59c2
Everett Pompeii <epompeii@protonmail.com>
2022-11-28 16:02:22 +0000

    gitpod

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,15 +5,13 @@
 sudo apt-get update -q
 sudo apt-get install -yq netcat-openbsd sqlite3
 
+rustup target add wasm32-unknown-unknown
 rustup toolchain install nightly
 rustup component add rust-src
 rustup component add rustfmt
 rustup component add clippy
 cargo install cargo-udeps --locked
 cargo install diesel_cli --no-default-features --features sqlite --locked
-
-rustup target add wasm32-unknown-unknown
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
 curl -L https://fly.io/install.sh | sh
 echo "export FLYCTL_INSTALL=\"/home/gitpod/.fly\"" >> $HOME/.bash_profile
