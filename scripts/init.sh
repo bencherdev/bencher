78603a62e24edf929a0da5e8ea9982dbec9515f8
Everett Pompeii <epompeii@protonmail.com>
2022-11-29 11:36:23 +0000

    gitpod

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,8 +10,10 @@ rustup toolchain install nightly
 rustup component add rust-src
 rustup component add rustfmt
 rustup component add clippy
+
 cargo install cargo-udeps --locked
 cargo install diesel_cli --no-default-features --features sqlite --locked
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
 
 curl -L https://fly.io/install.sh | sh
 echo "export FLYCTL_INSTALL=\"/home/gitpod/.fly\"" >> $HOME/.bash_profile
--- scripts/start_ui.sh
+++ scripts/start_ui.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+npm install --include=dev
+
 echo "Waiting for API server"
 while ! nc -z localhost 61016; do
   sleep .1
