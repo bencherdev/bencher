6de7eccee07c6bc45813fdfc63220fc1989b8dc3
Everett Pompeii <epompeii@protonmail.com>
2022-12-03 17:19:07 +0000

    BranchName

--- lib/bencher_json/src/project/branch.rs
+++ lib/bencher_json/src/project/branch.rs
@@ -1,24 +1,35 @@
-use bencher_valid::Slug;
+use bencher_valid::{BranchName, Slug};
 use once_cell::sync::Lazy;
 #[cfg(feature = "schema")]
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-pub const BRANCH_MAIN: &str = "main";
-static BRANCH_MAIN_SLUG: Lazy<Option<Slug>> = Lazy::new(|| BRANCH_MAIN.parse().ok());
+pub const BRANCH_MAIN_STR: &str = "main";
+static BRANCH_MAIN: Lazy<BranchName> = Lazy::new(|| {
+    BRANCH_MAIN_STR
+        .parse()
+        .expect("Failed to parse branch name.")
+});
+static BRANCH_MAIN_SLUG: Lazy<Option<Slug>> = Lazy::new(|| {
+    Some(
+        BRANCH_MAIN_STR
+            .parse()
+            .expect("Failed to parse branch slug."),
+    )
+});
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[cfg_attr(feature = "schema", derive(JsonSchema))]
 pub struct JsonNewBranch {
-    pub name: String,
+    pub name: BranchName,
     pub slug: Option<Slug>,
 }
 
 impl JsonNewBranch {
     pub fn main() -> Self {
         Self {
-            name: BRANCH_MAIN.into(),
+            name: BRANCH_MAIN.clone(),
             slug: BRANCH_MAIN_SLUG.clone(),
         }
     }
@@ -29,6 +40,6 @@ impl JsonNewBranch {
 pub struct JsonBranch {
     pub uuid: Uuid,
     pub project: Uuid,
-    pub name: String,
+    pub name: BranchName,
     pub slug: Slug,
 }
