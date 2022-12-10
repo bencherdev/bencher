0fa8fa98985cf32b43e139fbe3abd6ff2905bd90
Everett Pompeii <epompeii@protonmail.com>
2022-12-10 15:42:25 +0000

    report

--- services/ui/src/components/console/config/reports.tsx
+++ services/ui/src/components/console/config/reports.tsx
@@ -10,14 +10,23 @@ const reportsConfig = {
       buttons: [{ kind: Button.REFRESH }],
     },
     table: {
-      url: (path_params) => `${BENCHER_API_URL()}/v0/projects/${path_params?.project_slug}/reports`,
+      url: (path_params) =>
+        `${BENCHER_API_URL()}/v0/projects/${path_params?.project_slug}/reports`,
       add: {
         path: (_pathname) => "/docs/how-to/run-a-report",
         text: "Run a Report",
       },
       row: {
         key: "start_time",
-        items: [{}, {}, {}, {}],
+        items: [
+          {
+            kind: Row.TEXT,
+            key: "adapter",
+          },
+          {},
+          {},
+          {},
+        ],
         button: {
           text: "View",
           path: viewUuidPath,
@@ -32,12 +41,33 @@ const reportsConfig = {
       path: parentPath,
     },
     deck: {
-      url: (path_params) => `${BENCHER_API_URL()}/v0/projects/${path_params?.project_slug}/reports/${path_params?.report_uuid}`,
+      url: (path_params) =>
+        `${BENCHER_API_URL()}/v0/projects/${
+          path_params?.project_slug
+        }/reports/${path_params?.report_uuid}`,
       cards: [
         {
           kind: Card.FIELD,
-          label: "Report Date Time",
+          label: "Report UUID",
+          key: "uuid",
+          display: Display.RAW,
+        },
+        {
+          kind: Card.FIELD,
+          label: "Report Start Time",
           key: "start_time",
+          display: Display.RAW,
+        },
+        {
+          kind: Card.FIELD,
+          label: "Report End Time",
+          key: "end_time",
+          display: Display.RAW,
+        },
+        {
+          kind: Card.FIELD,
+          label: "Results Adapter",
+          key: "adapter",
           display: Display.RAW,
         },
       ],
