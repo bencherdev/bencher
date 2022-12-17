1d0f260628d927c511c41fe781f5d5e5142a1422
Everett Pompeii <epompeii@protonmail.com>
2022-12-17 14:38:01 +0000

    pub_benchmarks

--- services/api/src/endpoints/project/benchmarks.rs
+++ services/api/src/endpoints/project/benchmarks.rs
@@ -1,7 +1,6 @@
 use std::sync::Arc;
 
 use bencher_json::{JsonBenchmark, ResourceId};
-use bencher_rbac::project::Permission;
 use diesel::{expression_methods::BoolExpressionMethods, ExpressionMethods, QueryDsl, RunQueryDsl};
 use dropshot::{endpoint, HttpError, Path, RequestContext};
 use schemars::JsonSchema;
@@ -11,7 +10,7 @@ use uuid::Uuid;
 use crate::{
     context::Context,
     endpoints::{
-        endpoint::{response_ok, ResponseOk},
+        endpoint::{pub_response_ok, response_ok, ResponseOk},
         Endpoint, Method,
     },
     error::api_error,
@@ -57,34 +56,34 @@ pub async fn get_ls(
     rqctx: Arc<RequestContext<Context>>,
     path_params: Path<DirPath>,
 ) -> Result<ResponseOk<Vec<JsonBenchmark>>, HttpError> {
-    let auth_user = AuthUser::new(&rqctx).await?;
+    let auth_user = AuthUser::new(&rqctx).await.ok();
     let endpoint = Endpoint::new(BENCHMARK_RESOURCE, Method::GetLs);
 
     let json = get_ls_inner(
         rqctx.context(),
-        &auth_user,
+        auth_user.as_ref(),
         path_params.into_inner(),
         endpoint,
     )
     .await
     .map_err(|e| endpoint.err(e))?;
 
-    response_ok!(endpoint, json)
+    if auth_user.is_some() {
+        response_ok!(endpoint, json)
+    } else {
+        pub_response_ok!(endpoint, json)
+    }
 }
 
 async fn get_ls_inner(
     context: &Context,
-    auth_user: &AuthUser,
+    auth_user: Option<&AuthUser>,
     path_params: DirPath,
     endpoint: Endpoint,
 ) -> Result<Vec<JsonBenchmark>, ApiError> {
     let api_context = &mut *context.lock().await;
-    let query_project = QueryProject::is_allowed_resource_id(
-        api_context,
-        &path_params.project,
-        auth_user,
-        Permission::View,
-    )?;
+    let query_project =
+        QueryProject::is_allowed_public(api_context, &path_params.project, auth_user)?;
     let conn = &mut api_context.database;
 
     Ok(schema::benchmark::table
@@ -124,28 +123,32 @@ pub async fn get_one(
     rqctx: Arc<RequestContext<Context>>,
     path_params: Path<OnePath>,
 ) -> Result<ResponseOk<JsonBenchmark>, HttpError> {
-    let auth_user = AuthUser::new(&rqctx).await?;
+    let auth_user = AuthUser::new(&rqctx).await.ok();
     let endpoint = Endpoint::new(BENCHMARK_RESOURCE, Method::GetOne);
 
-    let json = get_one_inner(rqctx.context(), path_params.into_inner(), &auth_user)
-        .await
-        .map_err(|e| endpoint.err(e))?;
+    let json = get_one_inner(
+        rqctx.context(),
+        path_params.into_inner(),
+        auth_user.as_ref(),
+    )
+    .await
+    .map_err(|e| endpoint.err(e))?;
 
-    response_ok!(endpoint, json)
+    if auth_user.is_some() {
+        response_ok!(endpoint, json)
+    } else {
+        pub_response_ok!(endpoint, json)
+    }
 }
 
 async fn get_one_inner(
     context: &Context,
     path_params: OnePath,
-    auth_user: &AuthUser,
+    auth_user: Option<&AuthUser>,
 ) -> Result<JsonBenchmark, ApiError> {
     let api_context = &mut *context.lock().await;
-    let query_project = QueryProject::is_allowed_resource_id(
-        api_context,
-        &path_params.project,
-        auth_user,
-        Permission::View,
-    )?;
+    let query_project =
+        QueryProject::is_allowed_public(api_context, &path_params.project, auth_user)?;
     let conn = &mut api_context.database;
 
     schema::benchmark::table
