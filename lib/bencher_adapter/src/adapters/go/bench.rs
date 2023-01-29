43175f3a5dd1fa3f04e4125a168d80ad92c4d91f
Everett Pompeii <epompeii@protonmail.com>
2023-01-29 22:02:16 +0000

    test

--- lib/bencher_adapter/src/adapters/go/bench.rs
+++ lib/bencher_adapter/src/adapters/go/bench.rs
@@ -1,15 +1,16 @@
 use bencher_json::{BenchmarkName, JsonMetric};
 use nom::{
-    bytes::complete::{tag, take_until1},
+    bytes::complete::{tag, take_till1},
     character::complete::space1,
     combinator::{eof, map_res},
-    sequence::{delimited, tuple},
+    sequence::tuple,
     IResult,
 };
 
 use crate::{
-    adapters::util::{
-        parse_benchmark_name, parse_f64, parse_u64, parse_units, time_as_nanos, NomError,
+    adapters::{
+        print_ln,
+        util::{parse_benchmark_name, parse_f64, parse_u64, parse_units, time_as_nanos, NomError},
     },
     results::adapter_results::AdapterResults,
     Adapter, AdapterError,
@@ -36,14 +37,15 @@ impl Adapter for AdapterGoBench {
 fn parse_go(input: &str) -> IResult<&str, (BenchmarkName, JsonMetric)> {
     map_res(
         tuple((
-            take_until1(" "),
+            take_till1(|c| c == ' ' || c == '\t'),
+            print_ln,
             space1,
             parse_u64,
             space1,
             parse_go_bench,
             eof,
         )),
-        |(name, _, _iter, _, json_metric, _)| -> Result<(BenchmarkName, JsonMetric), NomError> {
+        |(name, _, _, _iter, _, json_metric, _)| -> Result<(BenchmarkName, JsonMetric), NomError> {
             let benchmark_name = parse_benchmark_name(name)?;
             Ok((benchmark_name, json_metric))
         },
@@ -71,7 +73,7 @@ pub(crate) mod test_go_bench {
 
     use crate::{
         adapters::test_util::{convert_file_path, validate_metrics},
-        Adapter, AdapterResults,
+        AdapterResults,
     };
 
     use super::{parse_go, AdapterGoBench};
@@ -83,20 +85,78 @@ pub(crate) mod test_go_bench {
 
     #[test]
     fn test_parse_go() {
-        for (index, (expected, input)) in [(
-            Ok((
-                "",
-                (
-                    "BenchmarkFib10-8".parse().unwrap(),
-                    JsonMetric {
-                        value: 325.0.into(),
-                        lower_bound: None,
-                        upper_bound: None,
-                    },
-                ),
-            )),
-            "BenchmarkFib10-8   		 					5000000		325 ns/op",
-        )]
+        for (index, (expected, input)) in [
+            (
+                Ok((
+                    "",
+                    (
+                        "BenchmarkFib10-8".parse().unwrap(),
+                        JsonMetric {
+                            value: 325.0.into(),
+                            lower_bound: None,
+                            upper_bound: None,
+                        },
+                    ),
+                )),
+                "BenchmarkFib10-8   		 					5000000		325 ns/op",
+            ),
+            (
+                Ok((
+                    "",
+                    (
+                        "BenchmarkFib20".parse().unwrap(),
+                        JsonMetric {
+                            value: 40_537.123.into(),
+                            lower_bound: None,
+                            upper_bound: None,
+                        },
+                    ),
+                )),
+                "BenchmarkFib20  	 	   					30000		40537.123 ns/op",
+            ),
+            (
+                Ok((
+                    "",
+                    (
+                        "BenchmarkFib/my_tabled_benchmark_-_10-8".parse().unwrap(),
+                        JsonMetric {
+                            value: 325.0.into(),
+                            lower_bound: None,
+                            upper_bound: None,
+                        },
+                    ),
+                )),
+                "BenchmarkFib/my_tabled_benchmark_-_10-8    	5000000		325 ns/op",
+            ),
+            (
+                Ok((
+                    "",
+                    (
+                        "BenchmarkFib/my_tabled_benchmark_-_20".parse().unwrap(),
+                        JsonMetric {
+                            value: 40_537.123.into(),
+                            lower_bound: None,
+                            upper_bound: None,
+                        },
+                    ),
+                )),
+                "BenchmarkFib/my_tabled_benchmark_-_20		30000		40537.123 ns/op",
+            ),
+            (
+                Ok((
+                    "",
+                    (
+                        "BenchmarkFib/my/tabled/benchmark_-_20".parse().unwrap(),
+                        JsonMetric {
+                            value: 40_537.456.into(),
+                            lower_bound: None,
+                            upper_bound: None,
+                        },
+                    ),
+                )),
+                "BenchmarkFib/my/tabled/benchmark_-_20		30001		40537.456 ns/op",
+            ),
+        ]
         .into_iter()
         .enumerate()
         {
@@ -104,63 +164,30 @@ pub(crate) mod test_go_bench {
         }
     }
 
-    // #[test]
-    // fn test_adapter_rust_one() {
-    //     let results = convert_rust_bench("one");
-    //     assert_eq!(results.inner.len(), 1);
-    //     validate_bench_metrics(&results, "tests::benchmark");
-    // }
+    #[test]
+    pub fn validate_adapter_go_bench() {
+        let results = convert_go_bench("five");
+        assert_eq!(results.inner.len(), 5);
 
-    // #[test]
-    // fn test_adapter_rust_ignore() {
-    //     let results = convert_rust_bench("ignore");
-    //     assert_eq!(results.inner.len(), 1);
-    //     validate_bench_metrics(&results, "tests::benchmark");
-    // }
+        let metrics = results.get("BenchmarkFib10-8").unwrap();
+        validate_metrics(metrics, 325.0, None, None);
 
-    // #[test]
-    // fn test_adapter_rust_many() {
-    //     let results = convert_rust_bench("many");
-    //     validate_adapter_rust_many(results);
-    // }
+        let metrics = results.get("BenchmarkFib20").unwrap();
+        validate_metrics(metrics, 40_537.123, None, None);
 
-    // pub fn validate_adapter_rust_many(results: AdapterResults) {
-    //     assert_eq!(results.inner.len(), 6);
-    //     validate_bench_metrics(&results, "tests::benchmark");
-    //     validate_bench_metrics(&results, "tests::other_benchmark");
-    //     validate_bench_metrics(&results, "tests::last_benchmark");
+        let metrics = results
+            .get("BenchmarkFib/my_tabled_benchmark_-_10-8")
+            .unwrap();
+        validate_metrics(metrics, 325.0, None, None);
 
-    //     let number = 1_000.0;
-    //     let metrics = results.get("tests::one_digit").unwrap();
-    //     validate_metrics(metrics, number, Some(0.0), Some(2000.0));
+        let metrics = results
+            .get("BenchmarkFib/my_tabled_benchmark_-_20")
+            .unwrap();
+        validate_metrics(metrics, 40_537.123, None, None);
 
-    //     let number = 22_000_000.0;
-    //     let metrics = results.get("tests::two_digit").unwrap();
-    //     validate_metrics(metrics, number, Some(0.0), Some(44_000_000.0));
-
-    //     let number = 333_000_000_000.0;
-    //     let metrics = results.get("tests::three_digit").unwrap();
-    //     validate_metrics(metrics, number, Some(0.0), Some(666_000_000_000.0));
-    // }
-
-    // #[test]
-    // fn test_adapter_rust_multi_target() {
-    //     let results = convert_rust_bench("multi_target");
-    //     assert_eq!(results.inner.len(), 2);
-    //     validate_bench_metrics(&results, "tests::benchmark");
-    //     validate_bench_metrics(&results, "tests::other_benchmark");
-    // }
-
-    // #[test]
-    // fn test_adapter_rust_failed() {
-    //     let contents = std::fs::read_to_string("./tool_output/rust/bench/failed.txt").unwrap();
-    //     let results = AdapterGoBench::parse(&contents).unwrap();
-    //     assert_eq!(results.inner.len(), 2);
-
-    //     let metrics = results.get("tests::benchmark_a").unwrap();
-    //     validate_metrics(metrics, 3_296.0, Some(2_775.0), Some(3_817.0));
-
-    //     let metrics = results.get("tests::benchmark_c").unwrap();
-    //     validate_metrics(metrics, 3_215.0, Some(2_859.0), Some(3_571.0));
-    // }
+        let metrics = results
+            .get("BenchmarkFib/my/tabled/benchmark_-_20")
+            .unwrap();
+        validate_metrics(metrics, 40_537.456, None, None);
+    }
 }
