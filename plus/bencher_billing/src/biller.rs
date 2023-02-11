3c7f48732b1d84a65e31f39b96d46070c2425bc4
Everett Pompeii <epompeii@protonmail.com>
2023-02-11 15:15:27 +0000

    customer

--- plus/bencher_billing/src/biller.rs
+++ plus/bencher_billing/src/biller.rs
@@ -1,5 +1,5 @@
 use bencher_valid::Secret;
-use stripe::Client;
+use stripe::{Client, CreateCustomer, Customer};
 
 use crate::BillingError;
 
@@ -11,5 +11,14 @@ impl Biller {
     pub fn new(secret_key: Secret) -> Self {
         let client = Client::new(secret_key);
         Self { client }
+    }
+
+    pub async fn new_customer(
+        &self,
+        create_customer: CreateCustomer,
+    ) -> Result<Customer, BillingError> {
+        Customer::create(&self, create_customer)
+            .await
+            .map_err(Into::into)
     }
 }
