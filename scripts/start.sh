15e402ef56978c9c867d977b4f9f78347afd736f
Everett Pompeii <epompeii@protonmail.com>
2023-02-22 14:21:27 +0000

    ci

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,8 +9,4 @@ curl -L https://fly.io/install.sh | sh
 echo "export FLYCTL_INSTALL=\"/home/gitpod/.fly\"" >> $HOME/.bash_profile
 echo "export PATH=\"/home/gitpod/.fly/bin:$PATH\"" >> $HOME/.bash_profile
 
-cd ~/.vscode-server/extensions/mtxr.sqltools-driver-sqlite-0.5.0
-npm install sqlite3@5.1.1
-cd -
-
 source ~/.bash_profile
