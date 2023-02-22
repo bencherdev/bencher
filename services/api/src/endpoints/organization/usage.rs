1f0c5d667ffa3c62da15f720b3fefa72576021f2
Everett Pompeii <epompeii@protonmail.com>
2023-02-22 16:46:38 +0000

    org_usage

--- services/api/src/endpoints/organization/usage.rs
+++ services/api/src/endpoints/organization/usage.rs
@@ -2,9 +2,10 @@
 
 use bencher_json::{organization::entitlements::JsonEntitlements, ResourceId};
 use bencher_rbac::organization::Permission;
-use chrono::NaiveDate;
+use chrono::serde::ts_seconds_option::deserialize as from_tsopt;
+use chrono::{DateTime, Utc};
 use diesel::{dsl::count, ExpressionMethods, JoinOnDsl, QueryDsl, RunQueryDsl};
-use dropshot::{endpoint, HttpError, Path, RequestContext};
+use dropshot::{endpoint, HttpError, Path, Query, RequestContext};
 use schemars::JsonSchema;
 use serde::Deserialize;
 
@@ -29,6 +30,14 @@ pub struct GetParams {
     pub organization: ResourceId,
 }
 
+#[derive(Deserialize, JsonSchema)]
+pub struct DirQuery {
+    #[serde(deserialize_with = "from_tsopt")]
+    pub start: Option<DateTime<Utc>>,
+    #[serde(deserialize_with = "from_tsopt")]
+    pub end: Option<DateTime<Utc>>,
+}
+
 #[allow(clippy::unused_async)]
 #[endpoint {
     method = OPTIONS,
@@ -38,6 +47,7 @@ pub struct GetParams {
 pub async fn options(
     _rqctx: RequestContext<Context>,
     _path_params: Path<GetParams>,
+    _query_params: Query<DirQuery>,
 ) -> Result<CorsResponse, HttpError> {
     Ok(get_cors::<Context>())
 }
@@ -50,13 +60,19 @@ pub async fn options(
 pub async fn get(
     rqctx: RequestContext<Context>,
     path_params: Path<GetParams>,
+    query_params: Query<DirQuery>,
 ) -> Result<ResponseOk<JsonEntitlements>, HttpError> {
     let auth_user = AuthUser::new(&rqctx).await?;
     let endpoint = Endpoint::new(USAGE_RESOURCE, Method::GetOne);
 
-    let json = get_inner(rqctx.context(), path_params.into_inner(), &auth_user)
-        .await
-        .map_err(|e| endpoint.err(e))?;
+    let json = get_inner(
+        rqctx.context(),
+        path_params.into_inner(),
+        query_params.into_inner(),
+        &auth_user,
+    )
+    .await
+    .map_err(|e| endpoint.err(e))?;
 
     response_ok!(endpoint, json)
 }
@@ -64,6 +80,7 @@ pub async fn get(
 async fn get_inner(
     context: &Context,
     path_params: GetParams,
+    query_params: DirQuery,
     auth_user: &AuthUser,
 ) -> Result<JsonEntitlements, ApiError> {
     let api_context = &mut *context.lock().await;
@@ -71,28 +88,12 @@ async fn get_inner(
 
     // Get the organization
     let query_org = QueryOrganization::from_resource_id(conn, &path_params.organization)?;
-
     // Check to see if user has permission to manage a project within the organization
     api_context
         .rbac
         .is_allowed_organization(auth_user, Permission::Manage, &query_org)?;
 
-    // TODO this should actually be the current billing period
-    let year = 2022;
-    let month = 1;
-    let day = 1;
-
-    let hours = 0;
-    let min = 0;
-    let sec = 0;
-
-    let start_billing_period = NaiveDate::from_ymd_opt(year, month, day)
-        .ok_or(ApiError::BadDate(year, month, day))?
-        .and_hms_opt(hours, min, sec)
-        .ok_or(ApiError::BadTime(hours, min, sec))?
-        .timestamp();
-
-    let metrics_used = schema::metric::table
+    let mut query = schema::metric::table
         .left_join(schema::perf::table.on(schema::metric::perf_id.eq(schema::perf::id)))
         .left_join(
             schema::benchmark::table.on(schema::perf::benchmark_id.eq(schema::benchmark::id)),
@@ -100,7 +101,21 @@ async fn get_inner(
         .left_join(schema::project::table.on(schema::benchmark::project_id.eq(schema::project::id)))
         .filter(schema::project::organization_id.eq(query_org.id))
         .inner_join(schema::report::table.on(schema::perf::report_id.eq(schema::report::id)))
-        .filter(schema::report::start_time.ge(start_billing_period))
+        .into_boxed();
+
+    let DirQuery { start, end } = query_params;
+
+    let start_time_nanos = start.as_ref().map(chrono::DateTime::timestamp_nanos);
+    if let Some(start_time) = start_time_nanos {
+        query = query.filter(schema::report::end_time.ge(start_time));
+    }
+
+    let end_time_nanos = end.as_ref().map(chrono::DateTime::timestamp_nanos);
+    if let Some(end_time) = end_time_nanos {
+        query = query.filter(schema::report::end_time.le(end_time));
+    }
+
+    let metrics_used = query
         .select(count(schema::metric::value))
         .first::<i64>(conn)
         .map_err(api_error!())?;
