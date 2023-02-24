a8b7f0a02687205ed0ebe6bb62208daab8f58e50
Everett Pompeii <epompeii@protonmail.com>
2023-02-24 20:16:03 +0000

    fibonacci

---
+++ examples/python/README.md
@@ -0,0 +1,10 @@
+```python
+pipenv shell
+python fbf.py
+```
+
+or
+
+```python
+pipenv run python fbf.py
+```
--- examples/python/fbf.py
+++ examples/python/fbf.py
@@ -1,12 +1,23 @@
 def fizz_buzz_fibonacci(n):
     response = ''
-
-    if not n%3:
+    if not n % 3:
         response += 'Fizz'
-    if not n%5:
+    if not n % 5:
         response += 'Buzz'
+    if not n % 7:
+        return fibonacci(n)
+    return response
 
-   return response
 
-for n in range(0,max):
-    print fizz_buzz_fibonacci(n)
+def fibonacci(n):
+    if n < 2:
+        return n
+    fib_prev = 1
+    fib = 1
+    for _ in range(2, n):
+        fib_prev, fib = fib, fib + fib_prev
+    return fib
+
+
+for n in range(0, 100):
+    print(fizz_buzz_fibonacci(n))
