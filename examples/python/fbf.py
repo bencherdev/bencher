99476f4e6878b21ee6252d5c4772fc1505235f58
Everett Pompeii <epompeii@protonmail.com>
2023-02-24 21:44:30 +0000

    recursive

--- examples/python/fbf.py
+++ examples/python/fbf.py
@@ -1,23 +1,48 @@
-def fizz_buzz_fibonacci(n):
+# Example Python benchmarking
+
+def fizz(n):
+    if not n % 3:
+        return 'Fizz'
+    return None
+
+
+def fizz_buzz(n):
     response = ''
     if not n % 3:
         response += 'Fizz'
     if not n % 5:
         response += 'Buzz'
+    return response if response else None
+
+
+def fizz_buzz_fibonacci(n):
     if not n % 7:
         return fibonacci(n)
-    return response
+
+    response = ''
+    if not n % 3:
+        response += 'Fizz'
+    if not n % 5:
+        response += 'Buzz'
+    return response if response else None
 
 
 def fibonacci(n):
     if n < 2:
         return n
-    fib_prev = 1
-    fib = 1
-    for _ in range(2, n):
-        fib_prev, fib = fib, fib + fib_prev
-    return fib
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
-for n in range(0, 100):
+def fibonacci_memo():
+    pad = {0: 0, 1: 1}
+
+    def func(n):
+        if n not in pad:
+            pad[n] = func(n-1) + func(n-2)
+        return pad[n]
+    return func
+
+
+for n in range(0, 42):
     print(fizz_buzz_fibonacci(n))
