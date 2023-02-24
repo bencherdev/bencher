1c125a93f8c552ddfa9fde34a48a94ebe44a1a2b
Everett Pompeii <epompeii@protonmail.com>
2023-02-24 01:25:36 +0000

    start

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,8 +9,8 @@ curl -L https://fly.io/install.sh | sh
 echo "export FLYCTL_INSTALL=\"/home/gitpod/.fly\"" >> $HOME/.bash_profile
 echo "export PATH=\"/home/gitpod/.fly/bin:$PATH\"" >> $HOME/.bash_profile
 
-cd ~/.local/share/vscode-sqltools
-npm install sqlite3@5.1.1
-cd -
+# cd ~/.local/share/vscode-sqltools
+# npm install sqlite3@5.1.1
+# cd -
 
 source ~/.bash_profile
