04b115e6a3473198b3c59bb26851a93f3bee3f1e
Everett Pompeii <epompeii@protonmail.com>
2023-03-04 14:41:11 +0000

    chromes

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,13 +3,13 @@
 ./scripts/githooks.sh
 
 sudo apt-get update -q
-sudo apt-get install -yq netcat-openbsd sqlite3 chromium-browser
+sudo apt-get install -yq netcat-openbsd sqlite3
 
 sudo apt-get install -y gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libwayland-server0 libgbm1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils libu2f-udev
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get -fy install
-rm google-chrome-stable_current_amd64.deb
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i google-chrome-stable_current_amd64.deb
+# sudo apt-get -fy install
+# rm google-chrome-stable_current_amd64.deb
 
 curl -L https://fly.io/install.sh | sh
 echo "export FLYCTL_INSTALL=\"/home/gitpod/.fly\"" >> $HOME/.bash_profile
