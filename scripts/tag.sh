fb5ff0dfef02a6f51730e4344cdaff3550504658
Everett Pompeii <epompeii@protonmail.com>
2023-03-17 13:32:33 +0000

    action

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Generate the Bencher CLI GitHub Action
+cd ./services/action
+npm install --include=dev
+npm run build
+cd -
+
 # Generate the API docs from the server
 cd ./services/api
 cargo run --features swagger
@@ -7,6 +13,7 @@ cd -
 
 git add Cargo.toml
 git add Cargo.lock
+git add ./services/action/dist/index.js
 # If there was a change/the git tree is dirty add the updated file and commit
 SWAGGER=./services/ui/src/components/docs/api/swagger.json
 git diff --quiet $SWAGGER || git add $SWAGGER
