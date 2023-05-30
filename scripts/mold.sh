06caa0a8fd0ad4b30317c408874d4cbb8c3d4488
Everett Pompeii <epompeii@protonmail.com>
2023-05-30 17:38:14 -0400

    setup-mold

--- scripts/mold.sh
+++ scripts/mold.sh
@@ -3,8 +3,9 @@
 sudo apt-get update
 sudo apt-get install -y clang
 
-MOLD=mold-1.11.0-x86_64-linux
+MOLD_VERSION=1.11.0
+MOLD_DEFAULT=true
 
-curl -L $MOLD.tar.gz | sudo tar -C /opt -xz
-
-echo 'PATH="$PATH:/opt/$MOLD/bin"' >> ~/.profile
+echo "mold $MOLD_VERSION"
+curl -L --retry 10 --silent --show-error https://github.com/rui314/mold/releases/download/v$MOLD_VERSION/mold-$MOLD_VERSION-$(uname -m)-linux.tar.gz | sudo tar -C /usr/local --strip-components=1 -xzf -
+test $MOLD_DEFAULT = true -a "$(realpath /usr/bin/ld)" != /usr/local/bin/mold && sudo ln -sf /usr/local/bin/mold "$(realpath /usr/bin/ld)"; true
