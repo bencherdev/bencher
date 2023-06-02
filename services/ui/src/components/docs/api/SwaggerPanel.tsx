d74580717f9d79d754c4017bd32f956edd347e8c
Everett Pompeii <epompeii@protonmail.com>
2023-06-01 20:59:09 -0400

    swagger_server

--- services/ui/src/components/docs/api/SwaggerPanel.tsx
+++ services/ui/src/components/docs/api/SwaggerPanel.tsx
@@ -1,12 +1,33 @@
 import { createEffect } from "solid-js";
 import swagger from "./swagger.json";
 import { SwaggerUIBundle } from "swagger-ui-dist";
+import { BENCHER_API_URL } from "../../site/util";
 
-const SwaggerPanel = (props) => {
+const API_BENCHER_DEV = "https://api.bencher.dev";
+const API_URL = BENCHER_API_URL();
+const BENCHER_CLOUD = "Bencher Cloud";
+const BENCHER_SELF_HOSTED = "Bencher Self-Hosted";
+
+const is_bencher_cloud = API_URL === API_BENCHER_DEV;
+
+const SwaggerPanel = (_props) => {
 	createEffect(() => {
+		const swagger_spec = swagger;
+		// https://swagger.io/docs/specification/api-host-and-base-path/
+		swagger_spec.servers = [];
+		if (!is_bencher_cloud) {
+			swagger_spec.servers.push({
+				url: API_URL,
+				description: BENCHER_SELF_HOSTED,
+			});
+		}
+		swagger_spec.servers.push({
+			url: API_BENCHER_DEV,
+			description: BENCHER_CLOUD,
+		});
 		SwaggerUIBundle({
 			dom_id: "#swagger",
-			spec: swagger,
+			spec: swagger_spec,
 			layout: "BaseLayout",
 		});
 	});
@@ -14,18 +35,19 @@ const SwaggerPanel = (props) => {
 	return (
 		<div class="content">
 			<blockquote>
-				<h2 class="title">
-					🐰 Bencher Cloud API Endpoint:{" "}
+				<p>
+					🐰 {is_bencher_cloud ? BENCHER_CLOUD : BENCHER_SELF_HOSTED} API
+					Endpoint:{" "}
 					<code>
 						<a
-							href="https://api.bencher.dev/v0/server/version"
+							href={`${API_URL}/v0/server/version`}
 							target="_blank"
 							rel="noreferrer"
 						>
-							https://api.bencher.dev
+							{API_URL}
 						</a>
 					</code>
-				</h2>
+				</p>
 			</blockquote>
 			<hr />
 			<div id="swagger" />
