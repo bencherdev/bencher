c074de0fc4f395d6d76e66b6b805172476dda663
Everett Pompeii <epompeii@protonmail.com>
2023-06-07 10:10:58 -0400

    build_wasm

---
+++ scripts/build_wasm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# TODO user per-target profiles
+# In the mean time, we can use sed to change the value of wasm-opt on aaarch64
+# https://github.com/rust-lang/cargo/issues/4897
+
+CARGO_TOML="../../lib/bencher_valid/Cargo.toml"
+ARCH=$(uname -m)
+WASM_OPT="wasm-opt ="
+
+test $ARCH = "aarch64" && sed -i "s/$WASM_OPT true/$WASM_OPT false/g" $CARGO_TOML; true
+
+wasm-pack build ../../lib/bencher_valid --target web --features plus,wasm
+
+test $ARCH = "aarch64" && sed -i "s/$WASM_OPT false/$WASM_OPT true/g" $CARGO_TOML; true
