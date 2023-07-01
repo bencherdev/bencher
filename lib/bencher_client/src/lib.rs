a140bf03a4a3b1105d6b5ab0bc958dd5d8db9688
Everett Pompeii <epompeii@protonmail.com>
2023-07-01 17:00:30 -0400

    clippy

--- lib/bencher_client/src/lib.rs
+++ lib/bencher_client/src/lib.rs
@@ -1,4 +1,9 @@
-include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
+mod codegen {
+    #![allow(clippy::all)]
+    include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
+}
+
+pub use codegen::*;
 
 impl From<bencher_json::ResourceId> for types::ResourceId {
     fn from(resource_id: bencher_json::ResourceId) -> Self {
