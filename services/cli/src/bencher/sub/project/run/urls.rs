2e3a6eec1d057d03c6679173d5688f5641491585
Everett Pompeii <epompeii@protonmail.com>
2023-07-25 09:34:53 -0400

    boundary_param

--- services/cli/src/bencher/sub/project/run/urls.rs
+++ services/cli/src/bencher/sub/project/run/urls.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeMap;
 
-use bencher_json::{JsonPerfQuery, JsonReport, Slug};
+use bencher_json::{project::threshold::JsonThresholdStatistic, JsonPerfQuery, JsonReport, Slug};
 use url::Url;
 use uuid::Uuid;
 
@@ -18,11 +18,15 @@ impl BenchmarkUrls {
         let mut urls = BTreeMap::new();
         for iteration in &json_report.results {
             for result in iteration {
-                let metric_kind = result.metric_kind.slug.clone();
+                let boundary = result.threshold.as_ref().map(Into::into);
                 for benchmark_metric in &result.benchmarks {
                     urls.insert(
                         benchmark_metric.name.to_string(),
-                        benchmark_url.to_url(metric_kind.clone(), benchmark_metric.uuid),
+                        benchmark_url.to_url(
+                            result.metric_kind.slug.clone(),
+                            benchmark_metric.uuid,
+                            boundary.as_ref(),
+                        ),
                     );
                 }
             }
@@ -49,7 +53,7 @@ impl BenchmarkUrl {
         }
     }
 
-    fn to_url(&self, metric_kind: Slug, benchmark: Uuid) -> Url {
+    fn to_url(&self, metric_kind: Slug, benchmark: Uuid, boundary: Option<&BoundaryParam>) -> Url {
         let json_perf_query = JsonPerfQuery {
             metric_kind: metric_kind.into(),
             branches: vec![self.branch],
@@ -63,12 +67,41 @@ impl BenchmarkUrl {
         url.set_path(&format!("/console/projects/{}/perf", self.project_slug));
         url.set_query(Some(
             &json_perf_query
-                .to_query_string(&[
-                    // ("tab", Some("reports".into()))
-                ])
+                .to_query_string(
+                    &boundary
+                        .map(BoundaryParam::to_query_string)
+                        .unwrap_or(vec![]),
+                )
                 .unwrap_or_default(),
         ));
 
         url
+    }
+}
+
+struct BoundaryParam {
+    lower_boundary: bool,
+    upper_boundary: bool,
+}
+
+impl From<&JsonThresholdStatistic> for BoundaryParam {
+    fn from(json_threshold_statistic: &JsonThresholdStatistic) -> Self {
+        Self {
+            lower_boundary: json_threshold_statistic.statistic.lower_boundary.is_some(),
+            upper_boundary: json_threshold_statistic.statistic.upper_boundary.is_some(),
+        }
+    }
+}
+
+impl BoundaryParam {
+    fn to_query_string(&self) -> Vec<(&str, Option<String>)> {
+        let mut query_string = Vec::new();
+        if self.lower_boundary {
+            query_string.push(("lower_boundary", Some(true.to_string())));
+        }
+        if self.upper_boundary {
+            query_string.push(("upper_boundary", Some(true.to_string())));
+        }
+        query_string
     }
 }
