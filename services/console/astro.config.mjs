4f44148d93a469b7969a7b53e6560bfb14c68858
Everett Pompeii <epompeii@protonmail.com>
2023-07-30 09:45:46 -0400

    wasm

--- services/console/astro.config.mjs
+++ services/console/astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import solidJs from "@astrojs/solid-js";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import wasmPack from "vite-plugin-wasm-pack";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [sitemap(), mdx(), solidJs()],
+  vite: {
+    plugins: [wasmPack("../../lib/bencher_valid")],
+  },
+});
