75afd00330399e3a2878020f6570f62ad7542151
Everett Pompeii <epompeii@protonmail.com>
2023-07-30 12:02:40 -0400

    footer

---
+++ services/console/src/util/ext.ts
@@ -0,0 +1,19 @@
+import swagger from "../content/swagger/swagger.json";
+
+export const BENCHER_CLOUD_API_URL: string = "https://api.bencher.dev";
+
+export const BENCHER_GITHUB_URL: string =
+	"https://github.com/bencherdev/bencher";
+
+export const BENCHER_CALENDLY_URL: string = "https://calendly.com/bencher/demo";
+
+export const BENCHER_CHAT_URL: string = "https://discord.gg/yGEsdUh7R4";
+
+export const BENCHER_LOGO_URL: string =
+	"https://s3.amazonaws.com/public.bencher.dev/bencher_navbar.png";
+
+export const BENCHER_USER_KEY: string = "BENCHER_USER";
+
+export const BENCHER_TITLE = "Bencher - Continuous Benchmarking";
+
+export const BENCHER_VERSION = `${swagger?.info?.version}`;
