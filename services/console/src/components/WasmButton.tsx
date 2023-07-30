88b7034da7ad8667682451c0b7d2d21c9bf7d21f
Everett Pompeii <epompeii@protonmail.com>
2023-07-30 11:46:58 -0400

    port

--- services/console/src/components/WasmButton.tsx
+++ services/console/src/components/WasmButton.tsx
@@ -1,6 +1,6 @@
 import bencher_valid_init, { is_valid_jwt } from "bencher_valid";
 
 
-const WasmButton = () => <button onClick={(e) => bencher_valid_init().then(() => console.log(is_valid_jwt("bad")))}>Validate JWT</button>;
+const WasmButton = () => <button class="button" onClick={(e) => bencher_valid_init().then(() => console.log(is_valid_jwt("bad")))}>Validate JWT</button>;
 
 export default WasmButton;
