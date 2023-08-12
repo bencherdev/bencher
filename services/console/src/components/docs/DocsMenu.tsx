6b6280d60e71a69091fde95df3c02699f9481988
Everett Pompeii <epompeii@protonmail.com>
2023-08-12 16:07:55 -0400

    docs

---
+++ services/console/src/components/docs/DocsMenu.tsx
@@ -0,0 +1,32 @@
+import { authUser } from "../../util/auth";
+
+enum Section {
+	TOKENS = "tokens",
+	SETTINGS = "settings",
+	HELP = "help",
+}
+
+const UserMenu = () => {
+	const user = authUser();
+	const path = (section: Section) =>
+		`/console/users/${user?.user?.slug}/${section}`;
+
+	return (
+		<aside class="menu is-sticky">
+			<p class="menu-label">User</p>
+			<ul class="menu-list">
+				<li>
+					<a href={path(Section.TOKENS)}>API Tokens</a>
+				</li>
+				<li>
+					<a href={path(Section.SETTINGS)}>Settings</a>
+				</li>
+				<li>
+					<a href={path(Section.HELP)}>Help</a>
+				</li>
+			</ul>
+		</aside>
+	);
+};
+
+export default UserMenu;
