8208537ae85998dcd807da8d9c53434f105911c5
Everett Pompeii <epompeii@protonmail.com>
2023-08-21 09:11:38 -0400

    partytown

--- services/console/astro.config.mjs
+++ services/console/astro.config.mjs
@@ -5,12 +5,14 @@ import mdx from "@astrojs/mdx";
 import wasmPack from "vite-plugin-wasm-pack";
 import remarkGfm from "remark-gfm";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
 	// https://docs.astro.build/en/reference/configuration-reference/#site
 	site: "https://bencher.dev",
 	output: "hybrid",
-	integrations: [sitemap(), mdx(), solidJs()],
+	integrations: [sitemap(), mdx(), solidJs(), partytown()],
 	vite: {
 		plugins: [wasmPack("../../lib/bencher_valid")],
 	},
