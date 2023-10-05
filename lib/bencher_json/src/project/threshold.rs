dd8b44803133b6f900ec256a80ed5d80ced49bc6
Everett Pompeii <epompeii@protonmail.com>
2023-10-05 10:28:44 -0400

    default_upper_boundary

--- lib/bencher_json/src/project/threshold.rs
+++ lib/bencher_json/src/project/threshold.rs
@@ -35,7 +35,7 @@ impl JsonNewStatistic {
             min_sample_size: None,
             max_sample_size: Some(SampleSize::THIRTY),
             window: None,
-            lower_boundary: Some(Boundary::NINETY),
+            lower_boundary: Some(Boundary::EIGHTY_FIVE),
             upper_boundary: None,
         }
     }
@@ -47,7 +47,7 @@ impl JsonNewStatistic {
             max_sample_size: Some(SampleSize::THIRTY),
             window: None,
             lower_boundary: None,
-            upper_boundary: Some(Boundary::NINETY),
+            upper_boundary: Some(Boundary::EIGHTY_FIVE),
         }
     }
 }
