947073f59c91be32b6f068faa403706bbf9ab155
Everett Pompeii <epompeii@protonmail.com>
2023-10-25 20:28:49 -0400

    french

---
+++ services/console/src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const defaultLang = "en";
+export const showDefaultLang = false;
+
+export const languages = {
+	en: "English",
+	fr: "Français",
+};
+
+export const ui = {
+	en: {
+		"nav.home": "Home",
+		"nav.about": "About",
+		"nav.twitter": "Twitter",
+	},
+	fr: {
+		"nav.home": "Accueil",
+		"nav.about": "À propos",
+	},
+} as const;
