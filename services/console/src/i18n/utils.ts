8f96c00744af9ab5c9687a039728f5486f87f647
Everett Pompeii <epompeii@protonmail.com>
2023-10-26 08:59:57 -0400

    fr_docs

--- services/console/src/i18n/utils.ts
+++ services/console/src/i18n/utils.ts
@@ -1,4 +1,5 @@
 import { defaultLang, showDefaultLang, ui } from "./ui";
+import { getCollection } from "astro:content";
 
 export function getLangFromUrl(url: URL) {
 	const [, lang] = url.pathname.split("/");
@@ -16,4 +17,32 @@ export function useTranslatedPath(lang: keyof typeof ui) {
 	return function translatePath(path: string, l: string = lang) {
 		return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`;
 	};
+}
+
+//
+export async function getLangCollection(collection: string) {
+	const pages = await getCollection(collection);
+	const langPagesMap = pages
+		.map((page) => {
+			const [lang, ...slug] = page.id
+				.substring(0, page.id.lastIndexOf("."))
+				?.split("/");
+			page.slug = slug.join("/") || undefined;
+			return { lang, page };
+		})
+		.reduce((lpMap, langPage) => {
+			if (lpMap[langPage.lang]) {
+				lpMap[langPage.lang].push(langPage.page);
+			} else {
+				lpMap[langPage.lang] = [langPage.page];
+			}
+			return lpMap;
+		}, {});
+	const langPagesMapSorted = {};
+	for (const [key, value] of Object.entries(langPagesMap)) {
+		langPagesMapSorted[key] = value.sort(
+			(a, b) => a.data.sortOrder - b.data.sortOrder,
+		);
+	}
+	return langPagesMapSorted;
 }
