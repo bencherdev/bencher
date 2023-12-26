811e461a39fc2ccaf3494ceca6d307680fe23b21
Everett Pompeii <epompeii@protonmail.com>
2023-12-26 10:53:47 -0500

    auth_github

---
+++ services/console/src/components/auth/AuthGitHub.tsx
@@ -0,0 +1,33 @@
+import { createEffect } from "solid-js";
+import { authUser } from "../../util/auth";
+import { useNavigate, useSearchParams } from "../../util/url";
+
+const CODE_PARAM = "code";
+const INSTALLATION_ID_PARAM = "installation_id";
+const SETUP_ACTION_PARAM = "setup_action";
+
+interface Props {
+	apiUrl: string;
+}
+
+const AuthGitHub = (props: Props) => {
+	const [searchParams, _setSearchParams] = useSearchParams();
+	const user = authUser();
+	const navigate = useNavigate();
+
+	createEffect(() => {
+		if (user?.token) {
+			navigate("/console", { replace: true });
+		}
+		const code = searchParams[CODE_PARAM];
+		const installationId = searchParams[INSTALLATION_ID_PARAM];
+		const setupAction = searchParams[SETUP_ACTION_PARAM];
+		if (!code || !installationId || !setupAction) {
+			return;
+		}
+	});
+
+	return <></>;
+};
+
+export default AuthGitHub;
