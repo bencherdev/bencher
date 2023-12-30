2a1dfc0d89a0dde922d3bbf6497a406792abd028
Everett Pompeii <epompeii@protonmail.com>
2023-12-30 09:55:34 -0500

    rust_backtrace_windows

--- scripts/smoke_test.bat
+++ scripts/smoke_test.bat
@@ -1,11 +1,12 @@
 cd .\services\api
-START /B cargo run
+start /B cargo run
 
 cd ..\cli
 echo "Waiting for API server"
 :while
-  ping -n 1 127.0.0.1 >NUL
+  ping -n 5 127.0.0.1 >nul
   telnet localhost 61016 >nul 2>&1
   IF ERRORLEVEL 1 goto :while
 
-RUST_BACKTRACE=full cargo test --features seed --test seed -- --nocapture
+set RUST_BACKTRACE="full"
+cargo test --features seed --test seed -- --nocapture
