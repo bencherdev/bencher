ce5e472dc76eebe5623220ef1fb0797284553dcb
Everett Pompeii <epompeii@protonmail.com>
2023-12-30 15:47:56 -0500

    test_succeeded

--- scripts/smoke_test.bat
+++ scripts/smoke_test.bat
@@ -7,7 +7,7 @@ cd ..\cli
 echo "Waiting for API server"
 
 :while
-powershell -Command "if (Test-NetConnection -ComputerName localhost -Port 61016) { exit } else { Start-Sleep -Seconds 1 }"
+powershell -Command "$connection = Test-NetConnection -ComputerName localhost -Port 61016; if ($connection.TcpTestSucceeded) { exit 0 } else { exit 1 }"
 if %errorlevel% neq 0 goto while
 
 set RUST_BACKTRACE=full
