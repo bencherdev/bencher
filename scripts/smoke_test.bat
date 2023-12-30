b44929c07a59efb2c61309d7cdceed2b58449441
Everett Pompeii <epompeii@protonmail.com>
2023-12-30 08:02:40 -0500

    windows_timeout

--- scripts/smoke_test.bat
+++ scripts/smoke_test.bat
@@ -4,7 +4,8 @@ START /B cargo run
 cd ..\cli
 echo "Waiting for API server"
 :while
-  timeout 1
-  if ! telnet localhost 61016 >nul 2>&1 goto while
+  ping -n 1 127.0.0.1 >NUL
+  telnet localhost 61016 >nul 2>&1
+  IF ERRORLEVEL 1 goto :while
 
 RUST_BACKTRACE=full cargo test --features seed --test seed -- --nocapture
