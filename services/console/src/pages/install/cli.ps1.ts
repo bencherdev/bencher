3ac40333094b6891405d9f22b06046389baf292b
Everett Pompeii <epompeii@protonmail.com>
2024-01-01 17:27:28 -0500

    install_cli

---
+++ services/console/src/pages/install/cli.ps1.ts
@@ -0,0 +1,9 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async function GET() {
+    return new Response("echo hello;", {
+        headers: {
+            'Content-Type': 'text/ps1',
+        },
+    });
+};
