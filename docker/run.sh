759717499ef9f1e5425c2d39eb5cd16fd2e8e9bf
Everett Pompeii <epompeii@protonmail.com>
2024-02-06 07:39:59 -0500

    internal_api_url

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-ARCH=${1:-$(uname -m)}
+function check_architecture() {
+    local arch=$(uname -m)
+    if [[ "$arch" == "x86_64" ]]; then
+        echo "x86_64"
+    elif [[ "$arch" == "aarch64" ]] || [[ "$arch" == "arm64" ]]; then
+        echo "arm64"
+    else
+        exit 1
+    fi
+}
+
+ARCH=${1:-$(check_architecture)}
 
 docker compose -f docker/docker-compose.$ARCH.yml up --build
-
-# docker compose -f docker/builder.docker-compose.yml build
-# docker compose -f docker/$KIND.docker-compose.yml up --build
--- services/console/src/env.d.ts
+++ services/console/src/env.d.ts
@@ -5,9 +5,13 @@ interface ImportMetaEnv {
 	readonly MODE: string;
 	// https://docs.astro.build/en/guides/environment-variables
 	readonly PUBLIC_IS_BENCHER_CLOUD?: string;
+	// https://support.google.com/analytics/answer/12270356?hl=en
 	readonly PUBLIC_GOOGLE_ANALYTICS_ID?: string;
 	// https://docs.astro.build/en/guides/integrations-guide/node/#runtime-environment-variables
 	readonly BENCHER_API_URL: string;
+	// https://docs.docker.com/desktop/networking/#use-cases-and-workarounds-for-all-platforms
+	readonly INTERNAL_API_URL: string;
+	// https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authenticating-to-the-rest-api-with-an-oauth-app
 	readonly GITHUB_CLIENT_ID: string;
 }
 
