c8f3608f4b0b22e712399124a800d0c0ea49582e
Everett Pompeii <epompeii@protonmail.com>
2024-02-07 07:57:40 -0500

    perf_img

--- services/console/src/components/console/perf/PerfHeader.tsx
+++ services/console/src/components/console/perf/PerfHeader.tsx
@@ -188,7 +188,7 @@ const ShareModal = (props: ShareProps) => {
 				title() ? title() : props.project()?.name
 			}" alt="${title() ? `${title()} for ` : ""}${
 				props.project()?.name
-			} - Bencher" /></a>`,
+			} - Bencher" width="1024" height="768" /></a>`,
 	);
 
 	const embed_tag = createMemo(
