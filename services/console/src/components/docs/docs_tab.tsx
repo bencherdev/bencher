6d660af907ebc195ef2afde9ce53189786c9fd9c
Everett Pompeii <epompeii@protonmail.com>
2024-02-12 07:26:08 -0500

    api_layout

--- services/console/src/components/docs/docs_tab.tsx
+++ services/console/src/components/docs/docs_tab.tsx
@@ -1,9 +1,11 @@
 export enum DocsTab {
 	Docs = "docs-tab",
+	Api = "api-tab",
 	Learn = "learn-tab",
 }
 
 export enum DocsMenuId {
 	Docs = "docs-menu",
+	Api = "api-menu",
 	Learn = "learn-menu",
 }
