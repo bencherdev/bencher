77f78b9bae3361a3ff0d53c76b585c0157638918
Everett Pompeii <epompeii@protonmail.com>
2024-02-27 07:04:30 -0500

    api_canonical

--- services/console/src/util/collection.ts
+++ services/console/src/util/collection.ts
@@ -14,4 +14,23 @@ enum Collection {
 
 export const ApiCollections = [Collection.organizations];
 
+export const collectionPath = (collection: Collection) => {
+	switch (collection) {
+		case Collection.legal:
+			return "legal";
+		case Collection.tutorial:
+			return "tutorial";
+		case Collection.how_to:
+			return "how-to";
+		case Collection.explanation:
+			return "explanation";
+		case Collection.reference:
+			return "reference";
+		case Collection.organizations:
+			return "organizations";
+		case Collection.rust:
+			return "benchmarking/rust";
+	}
+};
+
 export default Collection;
