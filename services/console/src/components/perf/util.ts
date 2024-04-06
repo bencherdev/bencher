ef0fa16bf746b35b9e9662356f472adf61091fc3
Everett Pompeii <epompeii@protonmail.com>
2024-04-06 07:50:33 -0400

    branch_selection

--- services/console/src/components/perf/util.ts
+++ services/console/src/components/perf/util.ts
@@ -3,4 +3,14 @@ import { PerfQueryKey } from "../../types/bencher";
 export const fetchSSR = async (url: string, timeout = 1_000) =>
 	await fetch(url, { signal: AbortSignal.timeout(timeout) });
 
-export const hasPerfImage = (url: URL) => [PerfQueryKey.Branches, PerfQueryKey.Testbeds, PerfQueryKey.Benchmarks, PerfQueryKey.Measures].reduce((acc, dimension) => acc && (url.searchParams.get(dimension)?.length ?? 0) > 0, true);
+export const hasPerfImage = (url: URL) =>
+	[
+		PerfQueryKey.Branches,
+		PerfQueryKey.Testbeds,
+		PerfQueryKey.Benchmarks,
+		PerfQueryKey.Measures,
+	].reduce(
+		(acc, dimension) =>
+			acc && (url.searchParams.get(dimension)?.length ?? 0) > 0,
+		true,
+	);
