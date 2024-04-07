b0e5e473e50514f54ab74914c33e9740bf96345f
Everett Pompeii <epompeii@protonmail.com>
2024-04-07 17:56:35 -0400

    twitter

--- services/console/src/components/docs/author.tsx
+++ services/console/src/components/docs/author.tsx
@@ -1,3 +1,12 @@
 export enum Author {
 	everett = "Everett Pompeii",
 }
+
+export const twitter = (author: Author) => {
+	switch (author) {
+		case Author.everett:
+			return "@epompeii";
+	}
+};
+
+export const TWITTER_BENCHER = "@bencherdev";
