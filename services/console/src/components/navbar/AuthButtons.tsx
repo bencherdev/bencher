4e0e17eafb9bacb66f6163a8bb004a3e50a13f82
Everett Pompeii <epompeii@protonmail.com>
2024-05-20 07:51:44 -0400

    back_to_console

---
+++ services/console/src/components/navbar/AuthButtons.tsx
@@ -0,0 +1,30 @@
+import { Show } from "solid-js";
+import { authUser } from "../../util/auth";
+import { decodePath } from "../../util/url";
+
+const AuthButtons = () => {
+	return (
+		<Show
+			when={authUser()?.token}
+			fallback={
+				<div class="buttons">
+					<a class="button" href="/auth/login">
+						Log in
+					</a>
+					<a class="button is-primary" href="/auth/signup">
+						<strong>Sign up</strong>
+					</a>
+				</div>
+			}
+		>
+			<a class="button" href={decodePath("/console")}>
+				<span class="icon has-text-primary">
+					<i class="fas fa-angle-left" aria-hidden="true" />
+				</span>
+				<span>Back to Console</span>
+			</a>
+		</Show>
+	);
+};
+
+export default AuthButtons;
--- services/console/src/components/navbar/id.tsx
+++ services/console/src/components/navbar/id.tsx
@@ -1,4 +1,2 @@
-export const BENCHER_NAVBAR_AUTH_ID = "bencher-navbar-auth";
-
 export const BENCHER_NAVBAR_BURGER_ID = "bencher-navbar-burger";
 export const BENCHER_NAVBAR_MENU_ID = "bencher-navbar-menu";
