3e1ce5afbf2c6ed683939c8ceba4fc6b0890ca6b
Everett Pompeii <epompeii@protonmail.com>
2024-05-26 07:05:32 -0400

    is_sorted

--- lib/bencher_rank/src/lib.rs
+++ lib/bencher_rank/src/lib.rs
@@ -1,5 +1,40 @@
 pub mod db;
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash, diesel::FromSqlRow, diesel::AsExpression)]
+#[derive(
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    Hash,
+    diesel::FromSqlRow,
+    diesel::AsExpression,
+)]
 #[diesel(sql_type = diesel::sql_types::BigInt)]
 pub struct Rank(i64);
+
+impl Rank {
+    pub fn is_sorted(ranks: &[Rank]) -> bool {
+        #[allow(clippy::indexing_slicing)]
+        ranks.windows(2).all(|w| {
+            assert!(w.len() == 2, "window size is not 2");
+            w[0] <= w[1]
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_sorted() {
+        let ranks = vec![Rank(1), Rank(2), Rank(3)];
+        assert!(Rank::is_sorted(&ranks));
+
+        let ranks = vec![Rank(1), Rank(3), Rank(2)];
+        assert!(!Rank::is_sorted(&ranks));
+    }
+}
