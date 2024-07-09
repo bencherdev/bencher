58b95943f8650cb4252c9a2980af3cfc91bb48bf
Everett Pompeii <epompeii@protonmail.com>
2024-07-09 12:41:46 -0400

    docker_rootless

--- services/cli/src/bencher/sub/docker/mod.rs
+++ services/cli/src/bencher/sub/docker/mod.rs
@@ -7,9 +7,9 @@ use container::Container;
 
 #[derive(thiserror::Error, Debug)]
 pub enum DockerError {
-    #[error("Failed to connect to Docker daemon. Are you sure Docker is running?\nError: {0}")]
+    #[error("Failed to connect to Docker daemon. Are you sure Docker is running?\nIf Docker is running, do you have the correct permissions?\nLearn more: https://docs.docker.com/engine/security/rootless/\nError: {0}")]
     Daemon(bollard::errors::Error),
-    #[error("Failed to ping the Docker daemon. Are you sure Docker is running?\nError: {0}")]
+    #[error("Failed to ping the Docker daemon. Are you sure Docker is running?\nIf Docker is running, do you have the correct permissions?\nLearn more: https://docs.docker.com/engine/security/rootless/\nError: {0}")]
     Ping(bollard::errors::Error),
     #[error("Failed to stop Docker container (`{container}`): {err}")]
     StopContainer {
