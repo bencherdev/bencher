ca90d83223cc14620706dc508febd7d62f6f9204
Everett Pompeii <epompeii@protonmail.com>
2024-08-23 11:28:38 -0400

    build_rs

--- services/benchers/src/main.rs
+++ services/benchers/src/main.rs
@@ -1,31 +1,3 @@
-use std::process::Command;
-
 fn main() {
-    install();
-}
-
-fn install() {
-    Command::new("cargo")
-        .args(&[
-            "install",
-            "--git",
-            "https://github.com/bencherdev/bencher",
-            "--branch",
-            "main",
-            "--locked",
-            "--force",
-            "bencher_cli",
-        ])
-        .status()
-        .expect("Failed to install bencher CLI");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::install;
-
-    #[test]
-    fn test_install() {
-        install();
-    }
+    println!("Use the `bencher` command");
 }
