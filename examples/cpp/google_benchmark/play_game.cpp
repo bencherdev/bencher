73a8e9b46f57d5850e96c977c54f089d31155c65
Everett Pompeii <epompeii@protonmail.com>
2024-11-02 21:41:58 -0400

    fix

--- examples/cpp/google_benchmark/play_game.cpp
+++ examples/cpp/google_benchmark/play_game.cpp
@@ -17,7 +17,7 @@ int game_v0()
     return 0;
 }
 
-std::string fizz_buzz(int n)
+std::string fizz_buzz_v1(int n)
 {
     if (n % 15 == 0)
     {
@@ -39,7 +39,7 @@ std::string fizz_buzz(int n)
 
 void play_game_v1(int n, bool should_print)
 {
-    std::string result = fizz_buzz(n);
+    std::string result = fizz_buzz_v1(n);
     if (should_print)
     {
         std::cout << result << std::endl;
@@ -97,11 +97,61 @@ std::string fizz_buzz_fibonacci_v2(int n)
     }
 }
 
-void play_game(int n, bool should_print)
+void play_game_v2(int n, bool should_print)
 {
     std::string result = fizz_buzz_fibonacci_v2(n);
     if (should_print)
     {
         std::cout << result << std::endl;
     }
+}
+
+bool is_fibonacci_number_v3(int n)
+{
+    int previous = 0, current = 1;
+    while (current < n)
+    {
+        int next = previous + current;
+        previous = current;
+        current = next;
+    }
+    return current == n;
+}
+
+std::string fizz_buzz_fibonacci_v3(int n)
+{
+    if (is_fibonacci_number_v3(n))
+    {
+        return "Fibonacci";
+    }
+    else if (n % 15 == 0)
+    {
+        return "FizzBuzz";
+    }
+    else if (n % 3 == 0)
+    {
+        return "Fizz";
+    }
+    else if (n % 5 == 0)
+    {
+        return "Buzz";
+    }
+    else
+    {
+        return std::to_string(n);
+    }
+}
+
+void play_game_v3(int n, bool should_print)
+{
+    std::string result = fizz_buzz_fibonacci_v3(n);
+    if (should_print)
+    {
+        std::cout << result << std::endl;
+    }
+}
+
+void play_game(int n, bool should_print)
+{
+    play_game_v3(n, should_print);
 }
--- examples/cpp/google_benchmark/play_game.h
+++ examples/cpp/google_benchmark/play_game.h
@@ -5,7 +5,9 @@
 
 void play_game_v1(int n, bool should_print);
 void game_v1();
-void play_game(int n, bool should_print);
+void play_game_v2(int n, bool should_print);
 void game_v2();
+void play_game_v3(int n, bool should_print);
+void play_game(int n, bool should_print);
 
 #endif // GAME_H
