fb090bd63c6848e7b83da4d6fcdcf0606230ac65
Everett Pompeii <epompeii@protonmail.com>
2024-11-02 22:12:36 -0400

    continuous_benchmarking

--- examples/cpp/google_benchmark/bench.sh
+++ examples/cpp/google_benchmark/bench.sh
@@ -1,2 +1,2 @@
 g++ -std=c++11 -isystem benchmark/include -Lbenchmark/build/src -lbenchmark -lpthread play_game.cpp benchmark_game.cpp -o benchmark_game
-./benchmark_game
+./benchmark_game --benchmark_format=json
