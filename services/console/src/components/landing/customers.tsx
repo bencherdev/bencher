3425bf62b4d18fdb727b74e7b041967c350bcaab
Everett Pompeii <epompeii@protonmail.com>
2025-01-11 13:41:09 -0600

    customers

---
+++ services/console/src/components/landing/customers.tsx
@@ -0,0 +1,55 @@
+interface Customer {
+	name: string;
+	icon: string;
+	github: string;
+	quote: string;
+}
+
+export const CUSTOMERS: Customer[][] = [
+	[
+		{
+			name: "",
+			icon: "",
+			github: "",
+			quote: "Bencher is like CodeCov for performance metrics.",
+		},
+		{
+			name: "",
+			icon: "",
+			github: "",
+			quote:
+				"I think I'm in heaven. Now that I'm starting to see graphs of performance over time automatically from tests I'm running in CI. It's like this whole branch of errors can be caught and noticed sooner.",
+		},
+	],
+	[
+		{
+			name: "",
+			icon: "",
+			github: "",
+			quote:
+				"Truthfully, I was thinking about building Bencher, or something like it, but you beat me to it!",
+		},
+		{
+			name: "",
+			icon: "",
+			github: "",
+			quote:
+				"I've been looking for a public service like this for about 10 years :)",
+		},
+	],
+	[
+		{
+			name: "",
+			icon: "",
+			github: "",
+			quote:
+				"I'm happy with how quickly I was able to get everything configured and working.",
+		},
+		{
+			name: "",
+			icon: "",
+			github: "",
+			quote: "Bencher's main ideas and concepts are really well designed.",
+		},
+	],
+];
