63bf34f02df5873e9769273e356832eb2fa3d52e
Everett Pompeii <epompeii@protonmail.com>
2025-01-23 22:13:49 -0600

    quotes

--- services/console/src/components/landing/customers.tsx
+++ services/console/src/components/landing/customers.tsx
@@ -14,41 +14,41 @@ export const CUSTOMERS: Customer[][] = [
 			quote: "Bencher is like CodeCov for performance metrics.",
 		},
 		{
-			name: "",
-			icon: "",
-			github: "",
+			name: "Price Clark",
+			icon: "https://s3.us-east-1.amazonaws.com/public.bencher.dev/customers/gpwclark.jpg",
+			github: "gpwclark",
 			quote:
 				"I think I'm in heaven. Now that I'm starting to see graphs of performance over time automatically from tests I'm running in CI. It's like this whole branch of errors can be caught and noticed sooner.",
 		},
 	],
 	[
 		{
-			name: "",
-			icon: "",
-			github: "",
+			name: "Joe Neeman",
+			icon: "https://s3.us-east-1.amazonaws.com/public.bencher.dev/customers/jneem.jpg",
+			github: "jneem",
 			quote:
-				"Truthfully, I was thinking about building Bencher, or something like it, but you beat me to it!",
+				"95% of the time I don't want to think about my benchmarks. But when I need to, Bencher ensures that I have the detailed historical record waiting there for me. It's fire-and-forget.",
 		},
 		{
-			name: "",
-			icon: "",
-			github: "",
+			name: "Jamie Wilkinson",
+			icon: "https://s3.us-east-1.amazonaws.com/public.bencher.dev/customers/jaqx0r.png",
+			github: "jaqx0r",
 			quote:
-				"I've been looking for a public service like this for about 10 years :)",
+				"I've been looking for a public service like Bencher for about 10 years :)",
 		},
 	],
 	[
 		{
-			name: "",
-			icon: "",
-			github: "",
+			name: "Weston Pace",
+			icon: "https://s3.us-east-1.amazonaws.com/public.bencher.dev/customers/westonpace.jpg",
+			github: "westonpace",
 			quote:
-				"I'm happy with how quickly I was able to get everything configured and working.",
+				"I'm happy with how quickly I was able to get Bencher configured and working.",
 		},
 		{
-			name: "",
-			icon: "",
-			github: "",
+			name: "Free Ekanayaka",
+			icon: "https://s3.us-east-1.amazonaws.com/public.bencher.dev/customers/freeekanayaka.jpg",
+			github: "freeekanayaka",
 			quote: "Bencher's main ideas and concepts are really well designed.",
 		},
 	],
