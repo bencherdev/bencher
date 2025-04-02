bd0d3c40994b2a86de1d2fbd5dea0056a4f10c08
Everett Pompeii <epompeii@protonmail.com>
2025-04-02 00:31:51 -0500

    language_box

--- services/console/src/components/docs/BenchmarkHarness.tsx
+++ services/console/src/components/docs/BenchmarkHarness.tsx
@@ -1,12 +1,5 @@
 import LinkFragment from "../site/LinkFragment.tsx";
 
-const LANGUAGES: string[][] = [
-	["devicon-cplusplus-line", "devicon-python-plain"],
-	["devicon-java-plain", "devicon-javascript-plain"],
-	["devicon-ruby-plain", "devicon-csharp-line"],
-	["devicon-go-original-wordmark", "devicon-rust-plain"],
-];
-
 const BenchmarkHarness = () => (
 	<div class="content">
 		<h2 class="title is-3">
@@ -14,24 +7,41 @@ const BenchmarkHarness = () => (
 			<LinkFragment fragment="select-your-benchmark-harness" />
 		</h2>
 		<br />
-		<div class="columns is-centered is-vcentered">
-			{LANGUAGES.map(([left_icon, right_icon]) => (
-				<div class="column is-one-quarter">
-					<div class="columns is-mobile">
-						<div class="column is-half">
-							<span class="icon has-text-primary is-large">
-								<i class={`${left_icon} fa-5x`} />
-							</span>
-						</div>
-						<br />
-						<div class="column is-half">
-							<span class="icon has-text-primary is-large">
-								<i class={`${right_icon} fa-5x`} />
-							</span>
-						</div>
-					</div>
-				</div>
-			))}
+		<div class="columns">
+			<div class="column is-half">
+				<LanguageBox icon="devicon-csharp-line" name="C#" />
+				<LanguageBox icon="devicon-cplusplus-plain" name="C++" />
+				<LanguageBox icon="devicon-go-original-wordmark" name="Go" />
+				<LanguageBox icon="devicon-java-plain" name="Java" />
+				<LanguageBox icon="devicon-javascript-plain" name="JavaScript" />
+			</div>
+			<div class="column is-half">
+				<LanguageBox icon="devicon-python-plain" name="Python" />
+				<LanguageBox icon="devicon-ruby-plain" name="Ruby" />
+				<LanguageBox icon="devicon-rust-plain" name="Rust" />
+				<LanguageBox icon="devicon-bash-plain" name="Shell" />
+				<LanguageBox icon="devicon-json-plain" name="JSON" />
+			</div>
+		</div>
+	</div>
+);
+
+const LanguageBox = (props: { icon: string; name: string }) => (
+	<div class="box columns is-mobile is-vcentered is-gapless">
+		<div class="column is-narrow">
+			<span class="icon has-text-primary is-large">
+				<i class={`${props.icon} fa-2x`} />
+			</span>
+		</div>
+		<div class="column is-narrow">
+			<div>{props.name}</div>
+		</div>
+		<div class="column is-fullwidth">
+			<div class="content has-text-right">
+				<span class="icon">
+					<i class="fas fa-caret-right" />
+				</span>
+			</div>
 		</div>
 	</div>
 );
