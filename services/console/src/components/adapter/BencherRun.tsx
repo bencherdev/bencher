01f29d4b5b644b68a53bde4d3c4b7bca371e6be2
Everett Pompeii <epompeii@protonmail.com>
2025-04-07 21:27:40 -0500

    no_div

--- services/console/src/components/adapter/BencherRun.tsx
+++ services/console/src/components/adapter/BencherRun.tsx
@@ -6,98 +6,95 @@ import { useBash } from "../os/operating_system";
 const BencherRun = (props) => {
 	const [bash] = createResource(useBash);
 
-	// This cannot be an empty tag (`<>`) in order for `fallback` to work properly
 	return (
-		<div>
-			<Switch
-				fallback={
-					<Show when={bash()} fallback={props.json_powershell}>
-						{props.json_bash}
-					</Show>
-				}
-			>
-				<Match when={adapter() === Adapter.RustBench}>
-					<Show when={bash()} fallback={props.rust_bench_powershell}>
-						{props.rust_bench_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.RustCriterion}>
-					<Show when={bash()} fallback={props.rust_criterion_powershell}>
-						{props.rust_criterion_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.RustIai}>
-					<Show when={bash()} fallback={props.rust_iai_powershell}>
-						{props.rust_iai_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.RustIaiCallgrind}>
-					<Show when={bash()} fallback={props.rust_iai_callgrind_powershell}>
-						{props.rust_iai_callgrind_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.CppGoogle}>
-					<Show when={bash()} fallback={props.cpp_google_powershell}>
-						{props.cpp_google_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.CppCatch2}>
-					<Show when={bash()} fallback={props.cpp_catch2_powershell}>
-						{props.cpp_catch2_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.GoBench}>
-					<Show when={bash()} fallback={props.go_bench_powershell}>
-						{props.go_bench_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.JavaJmh}>
-					<Show when={bash()} fallback={props.java_jmh_powershell}>
-						{props.java_jmh_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.CSharpDotNet}>
-					<Show when={bash()} fallback={props.csharp_dotnet_powershell}>
-						{props.csharp_dotnet_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.JsBenchmark}>
-					<Show when={bash()} fallback={props.js_benchmark_powershell}>
-						{props.js_benchmark_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.JsTime}>
-					<Show when={bash()} fallback={props.js_time_powershell}>
-						{props.js_time_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.PythonAsv}>
-					<Show when={bash()} fallback={props.python_asv_powershell}>
-						{props.python_asv_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.PythonPytest}>
-					<Show when={bash()} fallback={props.python_pytest_powershell}>
-						{props.python_pytest_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.RubyBenchmark}>
-					<Show when={bash()} fallback={props.ruby_benchmark_powershell}>
-						{props.ruby_benchmark_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.ShellHyperfine}>
-					<Show when={bash()} fallback={props.shell_hyperfine_powershell}>
-						{props.shell_hyperfine_bash}
-					</Show>
-				</Match>
-				<Match when={adapter() === Adapter.Json}>
-					<Show when={bash()} fallback={props.json_powershell}>
-						{props.json_bash}
-					</Show>
-				</Match>
-			</Switch>
-		</div>
+		<Switch
+			fallback={
+				<Show when={bash()} fallback={props.json_powershell}>
+					{props.json_bash}
+				</Show>
+			}
+		>
+			<Match when={adapter() === Adapter.RustBench}>
+				<Show when={bash()} fallback={props.rust_bench_powershell}>
+					{props.rust_bench_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.RustCriterion}>
+				<Show when={bash()} fallback={props.rust_criterion_powershell}>
+					{props.rust_criterion_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.RustIai}>
+				<Show when={bash()} fallback={props.rust_iai_powershell}>
+					{props.rust_iai_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.RustIaiCallgrind}>
+				<Show when={bash()} fallback={props.rust_iai_callgrind_powershell}>
+					{props.rust_iai_callgrind_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.CppGoogle}>
+				<Show when={bash()} fallback={props.cpp_google_powershell}>
+					{props.cpp_google_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.CppCatch2}>
+				<Show when={bash()} fallback={props.cpp_catch2_powershell}>
+					{props.cpp_catch2_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.GoBench}>
+				<Show when={bash()} fallback={props.go_bench_powershell}>
+					{props.go_bench_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.JavaJmh}>
+				<Show when={bash()} fallback={props.java_jmh_powershell}>
+					{props.java_jmh_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.CSharpDotNet}>
+				<Show when={bash()} fallback={props.csharp_dotnet_powershell}>
+					{props.csharp_dotnet_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.JsBenchmark}>
+				<Show when={bash()} fallback={props.js_benchmark_powershell}>
+					{props.js_benchmark_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.JsTime}>
+				<Show when={bash()} fallback={props.js_time_powershell}>
+					{props.js_time_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.PythonAsv}>
+				<Show when={bash()} fallback={props.python_asv_powershell}>
+					{props.python_asv_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.PythonPytest}>
+				<Show when={bash()} fallback={props.python_pytest_powershell}>
+					{props.python_pytest_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.RubyBenchmark}>
+				<Show when={bash()} fallback={props.ruby_benchmark_powershell}>
+					{props.ruby_benchmark_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.ShellHyperfine}>
+				<Show when={bash()} fallback={props.shell_hyperfine_powershell}>
+					{props.shell_hyperfine_bash}
+				</Show>
+			</Match>
+			<Match when={adapter() === Adapter.Json}>
+				<Show when={bash()} fallback={props.json_powershell}>
+					{props.json_bash}
+				</Show>
+			</Match>
+		</Switch>
 	);
 };
 
--- services/console/src/components/os/WindowsOnly.tsx
+++ services/console/src/components/os/WindowsOnly.tsx
@@ -2,9 +2,9 @@ import { createResource, Show } from "solid-js";
 import { getOperatingSystem, OperatingSystem } from "./operating_system";
 
 const WindowsOnly = (props: { children }) => {
-    const [os] = createResource(getOperatingSystem);
+	const [os] = createResource(getOperatingSystem);
 
-    return <Show when={os() === OperatingSystem.Windows}>{props.children}</Show>;
-}
+	return <Show when={os() === OperatingSystem.Windows}>{props.children}</Show>;
+};
 
 export default WindowsOnly;
