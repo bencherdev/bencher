89b4da39317806378dedb1e3d53fa7e35e4c20d7
Everett Pompeii <epompeii@protonmail.com>
2026-01-26 04:48:04 +0000

    claim_organization

--- services/console/src/components/auth/AuthRedirect.tsx
+++ services/console/src/components/auth/AuthRedirect.tsx
@@ -4,19 +4,20 @@ import type {
 	JsonAccept,
 	JsonAuthAck,
 	JsonAuthUser,
+	JsonOrganization,
 } from "../../types/bencher";
 import { authUser } from "../../util/auth";
 import { httpPost } from "../../util/http";
 import { NotifyKind, navigateNotify } from "../../util/notify";
 import { useSearchParams } from "../../util/url";
 import Redirect from "../site/Redirect";
-import { INVITE_PARAM, PLAN_PARAM } from "./auth";
+import { CLAIM_PARAM, INVITE_PARAM, PLAN_PARAM } from "./auth";
 
 const AuthRedirect = (props: { apiUrl?: string; path: string }) => {
 	const [searchParams, _setSearchParams] = useSearchParams();
 	const user = authUser();
 
-	const fetcher = createMemo(() => {
+	const acceptFetcher = createMemo(() => {
 		return {
 			user: user,
 			invite: searchParams[INVITE_PARAM],
@@ -56,10 +57,59 @@ const AuthRedirect = (props: { apiUrl?: string; path: string }) => {
 				);
 			});
 	};
-	const [_jsonAuth] = createResource<JsonAuthAck>(fetcher, acceptInvite);
+	const [_jsonAuth] = createResource<JsonAuthAck>(acceptFetcher, acceptInvite);
+
+	const claimFetcher = createMemo(() => {
+		return {
+			user: user,
+			organization_uuid: searchParams[CLAIM_PARAM],
+		};
+	});
+	const claimProject = async (fetcher: {
+		user: JsonAuthUser;
+		organization_uuid: undefined | string;
+	}) => {
+		const token = fetcher.user?.token;
+		const organization_uuid = fetcher.organization_uuid;
+		if (!props.apiUrl || !token || !organization_uuid) {
+			return null;
+		}
+		const path = `/v0/organizations/${organization_uuid}/claim`;
+		return await httpPost(props.apiUrl, path, token, {})
+			.then((_resp) => {
+				navigateNotify(
+					NotifyKind.OK,
+					"Project claimed!",
+					"/console",
+					null,
+					null,
+				);
+			})
+			.catch((error) => {
+				console.error(error);
+				Sentry.captureException(error);
+				navigateNotify(
+					NotifyKind.ERROR,
+					`Invalid claim attempt: ${error}`,
+					null,
+					null,
+					null,
+				);
+			});
+	};
+	const [_jsonOrganization] = createResource<JsonOrganization>(
+		claimFetcher,
+		claimProject,
+	);
 
 	return (
-		<Show when={authUser()?.token && !searchParams[INVITE_PARAM]}>
+		<Show
+			when={
+				authUser()?.token &&
+				!searchParams[INVITE_PARAM] &&
+				!searchParams[CLAIM_PARAM]
+			}
+		>
 			<Redirect path={props.path} />
 		</Show>
 	);
