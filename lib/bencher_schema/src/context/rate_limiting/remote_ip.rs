54c80cdfe67e05561ffe65fcc23023974a88cb4b
Everett Pompeii <epompeii@protonmail.com>
2026-02-02 05:10:51 +0000

    claude_review

--- lib/bencher_schema/src/context/rate_limiting/remote_ip.rs
+++ lib/bencher_schema/src/context/rate_limiting/remote_ip.rs
@@ -4,16 +4,17 @@ use http::HeaderMap;
 use slog::Logger;
 
 pub(super) fn remote_ip(log: &Logger, headers: &HeaderMap) -> Option<IpAddr> {
-    let remote_ip = remote_ip_inner(headers);
-    if let Some(ip) = &remote_ip {
-        #[cfg(feature = "otel")]
-        bencher_otel::ApiMeter::increment(bencher_otel::ApiCounter::UserIp);
-        slog::info!(log, "Remote IP"; "remote_ip" => %ip);
-    } else {
-        #[cfg(feature = "otel")]
-        bencher_otel::ApiMeter::increment(bencher_otel::ApiCounter::UserIpNotFound);
-    }
-    remote_ip
+    remote_ip_inner(headers)
+        .inspect(|remote_ip| {
+            #[cfg(feature = "otel")]
+            bencher_otel::ApiMeter::increment(bencher_otel::ApiCounter::UserIp);
+            slog::info!(log, "Remote IP"; "remote_ip" => %remote_ip);
+        })
+        .or_else(|| {
+            #[cfg(feature = "otel")]
+            bencher_otel::ApiMeter::increment(bencher_otel::ApiCounter::UserIpNotFound);
+            None
+        })
 }
 
 fn remote_ip_inner(headers: &HeaderMap) -> Option<IpAddr> {
