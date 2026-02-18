b5082370e0dfb5680afdcd70651d58a33944f9fd
Mathis Wellmann <wellmannmathis@gmail.com>
2026-02-18 06:02:57 +0100

    Nix flake (#671)
    [Nix] is a declarative approach to creating reproducible packages and
    artifacts and makes it easy for users to run the `bencher` software.
    
    ## Changes include:
    - Introduce the nix package for `bencher` which can be run directly from
    the github repo with `nix run github:bencherdev/bencher` or simply
    locally with `nix run`.
    - Also include nix package for `api`, which can also be run with `nix
    run .#api`.
    - The flake also includes a reproducible development shell which can be
    activated with `nix develop` or automatically with `direnv allow`, if
    `direnv` is installed.
    - CI: add `build_bencher_nix` step.
    - CI: add `build_api_nix` step.
    - CI: add `nix_lints` step, which runs `alejandra`, `statix` and
    `deadnix` to ensure clean nix code.
    
    [Nix]: https://nixos.org/

---
+++ flake.nix
@@ -0,0 +1,92 @@
+{
+  description = "Flake for bencher";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.minimal.override {
+          extensions = [
+            "rust-src"
+            "clippy"
+            "rust-analyzer"
+          ];
+        };
+
+        # Build inputs for the Rust project
+        buildInputs = with pkgs; [
+          rustToolchain
+          rust-bin.nightly.latest.rustfmt # Get nightly formatter.
+          clang
+          mold
+          pkg-config
+          fontconfig
+          binutils
+        ];
+        rust_tools = with pkgs; [
+          cargo-nextest
+        ];
+        nix_tools = with pkgs; [
+          alejandra # Nix code formatter
+          deadnix # Nix dead code checker
+          statix # Nix static code checker.
+        ];
+        mkPackage = pname:
+          pkgs.rustPlatform.buildRustPackage {
+            name = pname;
+            src = ./.;
+            cargoBuildFlags = ["--bin" "${pname}"];
+            cargoLock.lockFile = ./Cargo.lock;
+            doCheck = false;
+            inherit buildInputs;
+            nativeBuildInputs = buildInputs;
+            LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+          };
+      in {
+        # Build package with `nix build` or more specifically for example `nix run .#bencher`
+        packages = rec {
+          default = bencher;
+          bencher = mkPackage "bencher";
+          api = mkPackage "api";
+        };
+        # Enter reproducible development shell with `nix develop`
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = buildInputs ++ rust_tools ++ nix_tools;
+          };
+          # A more lightweight dev shell with only packages relevant for CI in it.
+          ci = pkgs.mkShell {
+            buildInputs = nix_tools;
+          };
+        };
+
+        # Run an app with `nix run` or more specifically e.g.: `nix run .#bencher`
+        apps = rec {
+          default = bencher;
+          # nix run .#bencher
+          bencher = flake-utils.lib.mkApp {
+            drv = self.packages.${system}.bencher;
+          };
+          # nix run .#api
+          api = flake-utils.lib.mkApp {
+            drv = self.packages.${system}.api;
+          };
+        };
+      }
+    );
+}
