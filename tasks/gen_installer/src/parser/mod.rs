0e6a19701cf75fc458cc35e6a91fae699b7cdccc
Everett Pompeii <epompeii@protonmail.com>
2026-03-26 14:46:46 +0000

    gen-installer runner

--- tasks/gen_installer/src/parser/mod.rs
+++ tasks/gen_installer/src/parser/mod.rs
@@ -2,7 +2,20 @@ use clap::{Parser, ValueEnum};
 
 #[derive(Parser, Debug)]
 pub struct TaskTemplate {
+    /// Product to generate installer for (cli or runner)
+    pub product: Option<TaskProduct>,
+    /// Template kind to generate (sh or ps1)
     pub template: Option<TaskTemplateKind>,
+}
+
+/// Product
+#[derive(ValueEnum, Debug, Clone, Copy)]
+#[clap(rename_all = "snake_case")]
+pub enum TaskProduct {
+    /// CLI installer
+    Cli,
+    /// Runner installer
+    Runner,
 }
 
 /// Template kind
